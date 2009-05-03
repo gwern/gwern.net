a98dbf73542daae8043c2c42a45835dc43f2ecb3
gwern0 <gwern0@gmail.com>
2009-05-03 13:30:00 +0000

    WebArchiver: fix hardwired email

--- plugins/WebArchiver.hs
+++ plugins/WebArchiver.hs
@@ -2,16 +2,14 @@
 -- to webcitation.org / https://secure.wikimedia.org/wikipedia/en/wiki/WebCite
 --
 -- Limitations:
--- * WebCite requires an email, for notifying the archiver whether the request
--- was successful or not. Currently I hardwire in my own personal email.
 -- * Only parses Markdown, not ReST or any other format; this is because 'readMarkdown'
--- is hardwired into it as well.
+-- is hardwired into it.
 --
 -- By: Gwern Branwen; placed in the public domain
 
-module Archiver (plugin) where
+module WebArchiver (plugin) where
 
-import Gitit.Interface (liftIO, processWithM, Plugin(PreCommitTransform), Inline(Link))
+import Gitit.Interface (askUser, liftIO, processWithM, uEmail, Plugin(PreCommitTransform), Inline(Link))
 import Control.Monad (when)
 import Network.URI (isURI)
 import Control.Concurrent (forkIO)
@@ -22,20 +20,24 @@ import Control.Monad.Trans (MonadIO)
 plugin :: Plugin
 plugin = PreCommitTransform archivePage 
 
-archivePage :: (MonadIO m) => String -> m String
-archivePage x = do let p = readMarkdown defaultParserState x
-                       -- force evaluation and archiving side-effects
-                   _p' <- liftIO $ processWithM archiveLinks p
+-- archivePage :: (MonadIO m) => String -> ReaderT (Config, Maybe User) (StateT IO) String
+archivePage x = do mbUser <- askUser
+                   let email = case mbUser of
+                        Nothing -> "nobody@mailinator.com"
+                        Just u  -> uEmail u 
+                   let p = readMarkdown defaultParserState x
+                   -- force evaluation and archiving side-effects
+                   _p' <- liftIO $ processWithM (archiveLinks email) p 
                    return x -- note: this is read-only - don't actually change page!
 
-archiveLinks :: Inline -> IO Inline
-archiveLinks x@(Link _ (ln, _)) = checkArchive ln >> return x
-archiveLinks x = return x
+archiveLinks :: String -> Inline -> IO Inline
+archiveLinks e x@(Link _ (uln, _)) = checkArchive e uln >> return x
+archiveLinks _ x = return x
 
 -- | Error check the URL.
-checkArchive :: (MonadIO m) => String -> m ()
-checkArchive a = when (isURI a) (liftIO $ archiveURL a)
+checkArchive :: (MonadIO m) => String -> String -> m ()
+checkArchive e u = when (isURI u) (liftIO $ archiveURL e u)
 
-archiveURL :: String -> IO ()
-archiveURL url = forkIO (openURL ("http://www.webcitation.org/archive?url=" ++ url ++ "&email=gwern0@gmail.com") >> return()) >> return ()
+archiveURL :: String -> String -> IO ()
+archiveURL eml url = writeFile "/home/gwern/foo" eml >> forkIO (openURL ("http://www.webcitation.org/archive?url=" ++ url ++ "&email=" ++ eml) >> return()) >> return ()
    where openURL = simpleHTTP . getRequest
