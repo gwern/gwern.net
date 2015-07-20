9821619e051928928213f64b8cf08ca7dc897766
gwern <gwern@gwern.net>
2015-07-20 19:13:54 -0400

    footnotes.js: make jQuery dependency explicit

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,7 +1,7 @@
 var Footnotes = {
     footnotetimeout: false,
     setup: function() {
-        var footnotelinks = $('.footnoteRef')
+        var footnotelinks = jQuery('.footnoteRef')
 
         footnotelinks.unbind('mouseover',Footnotes.footnoteover);
         footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
@@ -11,27 +11,27 @@ var Footnotes = {
     },
     footnoteover: function() {
         clearTimeout(Footnotes.footnotetimeout);
-        $('#footnotediv').stop();
-        $('#footnotediv').remove();
+        jQuery('#footnotediv').stop();
+        jQuery('#footnotediv').remove();
 
-        var id = $(this).attr('href').substr(1);
-        var position = $(this).offset();
+        var id = jQuery(this).attr('href').substr(1);
+        var position = jQuery(this).offset();
 
-        var div = $(document.createElement('div'));
+        var div = jQuery(document.createElement('div'));
         div.attr('id','footnotediv');
         div.bind('mouseover',Footnotes.divover);
         div.bind('mouseout',Footnotes.footnoteoout);
 
         var el = document.getElementById(id);
-        div.html('<div>'+$(el).html()+'</div>');
+        div.html('<div>'+jQuery(el).html()+'</div>');
 
-        $(document.body).append(div);
+        jQuery(document.body).append(div);
 
         var left = position.left;
-        if(left + 420  > $(window).width() + $(window).scrollLeft())
-            left = $(window).width() - 420 + $(window).scrollLeft();
+        if(left + 420  > jQuery(window).width() + jQuery(window).scrollLeft())
+            left = jQuery(window).width() - 420 + jQuery(window).scrollLeft();
         var top = position.top+20;
-        if(top + div.height() > $(window).height() + $(window).scrollTop())
+        if(top + div.height() > jQuery(window).height() + jQuery(window).scrollTop())
             top = position.top - div.height() - 15;
         div.css({
             left:left,
@@ -42,22 +42,22 @@ var Footnotes = {
     },
     footnoteoout: function() {
         Footnotes.footnotetimeout = setTimeout(function() {
-            $('#footnotediv').animate({
+            jQuery('#footnotediv').animate({
                 opacity: 0
             }, 600, function() {
-                $('#footnotediv').remove();
+                jQuery('#footnotediv').remove();
             });
         },100);
     },
     divover: function() {
         clearTimeout(Footnotes.footnotetimeout);
-        $('#footnotediv').stop();
-        $('#footnotediv').css({
+        jQuery('#footnotediv').stop();
+        jQuery('#footnotediv').css({
                 opacity: 1
         });
     }
 }
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
     Footnotes.setup();
 });
