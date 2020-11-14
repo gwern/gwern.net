4d5a712fdeffb183029f2a7428dfe0c713def459
Said Achmiz <achmizs@gmail.com>
2020-11-14 18:50:06 -0500

    Fixed extraneous-license-text bug in color-scheme-convert.php

--- css/color-scheme-convert.php
+++ css/color-scheme-convert.php
@@ -1,3 +1,5 @@
+<?php
+
 /*
 color-scheme-convert.php
 
@@ -21,8 +23,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-
-<?php
 
 if (!isset($argv[1]))
 	die;
