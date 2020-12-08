56f8b0789fef56da3c2e0856dc50435fd8b1be68
Said Achmiz <achmizs@gmail.com>
2020-12-08 13:47:08 -0500

    Add icons to dark mode selector widget

--- js/darkmode.js
+++ js/darkmode.js
@@ -177,10 +177,10 @@ function injectModeSelector() {
     document.querySelector("head").insertAdjacentHTML("beforeend", `<style id='mode-selector-styles'>
 	#mode-selector {
 		position: absolute;
-		right: 3px;
+		right: 0;
 		display: flex;
 		background-color: var(--GW-mode-selector-background-color);
-		padding: 0.125em 0.25em;
+		padding: 0.1em 0.25em 0.3em 0.25em;
 		border: 3px solid transparent;
 		opacity: 0.3;
 		transition:
@@ -200,13 +200,42 @@ function injectModeSelector() {
 		border: none;
 		background-color: transparent;
 		padding: 0.5em;
-		margin: 0;
+		margin: 0 0 0 1.375em;
 		line-height: 1;
 		font-family: Lucida Sans Unicode, Source Sans Pro, Helvetica, Trebuchet MS, sans-serif;
 		font-size: 0.75rem;
 		text-align: center;
 		color: var(--GW-mode-selector-button-text-color);
 		position: relative;
+		display: flex;
+	}
+	#mode-selector button::before {
+		width: 1.2em;
+		position: absolute;
+		left: -15px;
+		opacity: 0.35;
+		padding: 1px 0 0 0;
+	}
+	#mode-selector button.select-mode-auto::before {
+		content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z"></path></svg>');
+	}
+	#mode-selector button.select-mode-light::before {
+		content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path></svg>');
+		opacity: 0.45;
+		width: 1.3em;
+		padding: 0;
+		left: -15px;
+	}
+	#mode-selector button.select-mode-dark {
+		margin: 0 0 0 1.125em;
+	}
+	#mode-selector button.select-mode-dark::before {
+		content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"></path></svg>');
+		width: 1.15em;
+		left: -12px;
+	}
+	#mode-selector button:not(.selected):hover::before {
+		opacity: 1.0;
 	}
 	#mode-selector button:hover,
 	#mode-selector button.selected {
