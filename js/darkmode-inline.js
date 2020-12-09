da057c6ab2e96d7710964ac939ab424612cdb92c
Said Achmiz <achmizs@gmail.com>
2020-12-08 20:20:57 -0500

    Added explanatory comment to darkmode-inline.js

--- js/darkmode-inline.js
+++ js/darkmode-inline.js
@@ -2,6 +2,16 @@
 	See the file `darkmode.js` for license and more information.
 	*/
 
+/*	Dark mode: before anything else loads, check browser localStorage for dark 
+	mode preference and immediately toggle sets of CSS color variables/classes 
+	to avoid any 'flash of white' or delayed loading. Note: CSS falls back to 
+	the media-query browser/OS variable preference, so still works if JS is 
+	blocked! (The JS is only necessary for the theme switcher widget allowing 
+	'force light'/'force dark' options. If users block JS, set the dark mode 
+	preference, and are unhappy when they get dark mode, well, they made their 
+	bed and must lie in it.)
+	*/
+
 /*  Set specified color mode (auto, light, dark).
     */
 function setMode(modeOption) {
