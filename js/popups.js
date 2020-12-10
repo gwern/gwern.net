0e60899151cac80d4eb188d00a155ae9c955c232
Said Achmiz <achmizs@gmail.com>
2020-12-10 11:57:38 -0500

    Clarified log messages

--- js/popups.js
+++ js/popups.js
@@ -183,7 +183,7 @@ Extracts = {
             Extracts.popupContainer.removeEventListener("mouseup", Extracts.popupContainerClicked);
     },
     cleanup: () => {
-        GWLog("[popups.js] Cleaning up...", 1);
+        GWLog("[popups.js]  Cleaning up...", 1);
 
         //  Unbind event listeners.
         Extracts.unbind();
@@ -202,10 +202,10 @@ Extracts = {
         if (   (   ('ontouchstart' in document.documentElement)
         		&& GW.mediaQueries.mobileWidth.matches)
         	|| !GW.mediaQueries.hoverAvailable.matches) {
-            GWLog("[popups.js] Mobile client detected. Exiting.", 1);
+            GWLog("[popups.js]  Mobile client detected. Exiting.", 1);
             return;
         } else {
-            GWLog("[popups.js] Setting up...", 1);
+            GWLog("[popups.js]  Setting up...", 1);
         }
 
         //  Inject styles.
@@ -214,7 +214,7 @@ Extracts = {
         //  Inject popups container.
         let popupContainerParent = document.querySelector(Extracts.popupContainerParentSelector);
         if (!popupContainerParent) {
-            GWLog("[popups.js] Popup container parent element not found. Exiting.", 1);
+            GWLog("[popups.js]  Popup container parent element not found. Exiting.", 1);
             return;
         }
         popupContainerParent.insertAdjacentHTML("beforeend", `<div id='${Extracts.popupContainerID}' style='z-index: ${Extracts.popupContainerZIndex};'></div>`);
