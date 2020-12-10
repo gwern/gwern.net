27a1c01f9cd71b4ed6a1d669536dfc400d8fcb6c
Said Achmiz <achmizs@gmail.com>
2020-12-10 17:26:30 -0500

    Fix bug in build_css.php

--- build/build_css.php
+++ build/build_css.php
@@ -1,8 +1,9 @@
 <?php
 
 $css_dir = __DIR__ . "/../css";
+$build_dir = __DIR__;
 
-`php color-scheme-convert.php {$css_dir}/colors.css 1 "Lab" 0.55 > {$css_dir}/colors-dark.css`;
+`php {$build_dir}/color-scheme-convert.php {$css_dir}/colors.css 1 "Lab" 0.55 > {$css_dir}/colors-dark.css`;
 `cat {$css_dir}/colors-dark.css {$css_dir}/dark-mode-adjustments.css > {$css_dir}/dark-mode.css`;
 
 ?>
