8966cb2d06c136d484a35c153f25618b152d2a58
Said Achmiz <achmizs@gmail.com>
2020-12-26 03:19:57 -0500

    Fix bugs in pre-commit hook

--- build/pre-commit.hook.php
+++ build/pre-commit.hook.php
@@ -8,12 +8,12 @@ $static_dir = "{$build_dir}/..";
 
 if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/colors.css {$static_dir}/css/dark-mode-adjustments.css`)) {
 	require_once("{$build_dir}/build_css.php");
-	`git add {$static_dir}/.`;
+	`git add {$static_dir}/css/.`;
 }
 
 if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/colors.css {$static_dir}/css/initial.css {$static_dir}/css/dark-mode.css {$static_dir}/js/gw-inline.js {$static_dir}/js/darkmode-inline.js`)) {
 	require_once("{$build_dir}/build_includes.php");
-	`git add {$static_dir}/.`;
+	`git add {$static_dir}/includes/.`;
 }
 
 ?>
