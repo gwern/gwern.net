5d7ed7254e10bbe00fa792b12ffef1e2e1075d9e
gwern <gwern@gwern.net>
2021-03-07 23:28:21 -0500

    LA: +2 domains

--- build/LinkArchive.hs
+++ build/LinkArchive.hs
@@ -1,7 +1,7 @@
 {- LinkArchive.hs: module for generating Pandoc external links which are rewritten to a local static mirror which cannot break or linkrot—if something's worth linking, it's worth hosting!
 Author: Gwern Branwen
 Date: 2019-11-20
-When:  Time-stamp: "2021-03-03 10:18:00 gwern"
+When:  Time-stamp: "2021-03-07 18:21:30 gwern"
 License: CC-0
 -}
 
@@ -787,5 +787,8 @@ whiteList url
       , "www.nap.edu/catalog" -- low quality
       , "store.steampowered.com/app/" -- service
       , "www.traditionalmedicinals.com/" -- homepage
+      , "hiyo.jp" -- service/defunct
+      , "wkhtmltopdf.org" -- stable
+      , "www.sumatrapdfreader.org" -- stable
       ] = True
     | otherwise = False
