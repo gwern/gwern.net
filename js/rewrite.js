3931f5d698a54f211f6e3c8cb36550dae01ca288
Said Achmiz <achmizs@gmail.com>
2021-08-11 11:38:23 -0400

    Refactor broken anchorlink check code; disable redundant checks

--- js/rewrite.js
+++ js/rewrite.js
@@ -685,6 +685,57 @@ doWhenPageLoaded(() => {
     });
 }, { once: true });
 
+/**************************/
+/* BROKEN ANCHOR CHECKING */
+/**************************/
+/*	If a reader loads a page and the anchor ID/hash does not exist inside the page, 
+	fire off a request to the 404 page, whose logs are reviewed manually, 
+	with the offending page+anchor ID, for correction (either fixing an outdated 
+	link somewhere on gwern.net, or adding a span/div manually to the page to 
+	make old inbound links go where they ought to).
+	*/
+/*	Such broken anchors can reflect out of date cross-page references, or reflect 
+	incoming URLs from elsewhere on the Internet which are broken/outdated. 
+	(Within-page anchor links are checked statically at compile-time, and those 
+	errors should never exist.)
+	*/
+
+function reportBrokenAnchorLink(link) {
+	GWLog("reportBrokenAnchorLink", "rewrite.js", 1);
+
+	if (link.hash == "")
+		return;
+
+	let brokenHashLogRequestURL = new URL("https://" 
+										  + location.hostname 
+										  + "/static/404.html" 
+										  + "-error-" 
+										  + fixedEncodeURIComponent(link.pathname) 
+										  + "--" 
+										  + fixedEncodeURIComponent(link.hash.substr(1)));
+
+	GWLog("Reporting broken hash-anchor: " + brokenHashLogRequestURL, "rewrite.js", 1);
+
+	doAjax({ location: brokenHashLogRequestURL });
+}
+
+function brokenAnchorCheck() {
+	GWLog("brokenAnchorCheck", "rewrite.js", 1);
+
+	if (location.hash > "" && document.querySelector(location.hash) == null)
+		reportBrokenAnchorLink(location);
+
+	/*	Loop over internal page self-links and check that their targets exist; 
+		if they do not, report it via 404 like the broken anchors.
+		DEPRECATED DUE TO anchor-checker.php SCRIPT.
+		*/
+// 	document.querySelectorAll(".markdownBody a[href^='#']").forEach(anchor => {
+// 		if (document.querySelector(anchor.hash) == null)
+// 			reportBrokenAnchorLink(anchor);
+// 	});
+}
+doWhenDOMContentLoaded(brokenAnchorCheck);
+
 /********************/
 /* HASH REALIGNMENT */
 /********************/
@@ -726,31 +777,3 @@ doWhenPageLoaded(() => {
 /* Settings: 'prefetch' (loads HTML of target) after 800ms hover (desktop) or mouse-down-click (mobile); TODO: left in logging for testing during experiment */
 let t,e;const n=new Set,o=document.createElement("link"),z=o.relList&&o.relList.supports&&o.relList.supports("prefetch")&&window.IntersectionObserver&&"isIntersecting"in IntersectionObserverEntry.prototype,s="instantAllowQueryString"in document.body.dataset,a=true,r="instantWhitelist"in document.body.dataset,c="instantMousedownShortcut"in document.body.dataset,d=1111;let l=800,u=!1,f=!1,m=!1;if("instantIntensity"in document.body.dataset){const t=document.body.dataset.instantIntensity;if("mousedown"==t.substr(0,"mousedown".length))u=!0,"mousedown-only"==t&&(f=!0);else if("viewport"==t.substr(0,"viewport".length))navigator.connection&&(navigator.connection.saveData||navigator.connection.effectiveType&&navigator.connection.effectiveType.includes("2g"))||("viewport"==t?document.documentElement.clientWidth*document.documentElement.clientHeight<45e4&&(m=!0):"viewport-all"==t&&(m=!0));else{const e=parseInt(t);isNaN(e)||(l=e)}}if(z){const n={capture:!0,passive:!0};if(f||document.addEventListener("touchstart",function(t){e=performance.now();const n=t.target.closest("a");if(!h(n))return;v(n.href)},n),u?c||document.addEventListener("mousedown",function(t){const e=t.target.closest("a");if(!h(e))return;v(e.href)},n):document.addEventListener("mouseover",function(n){if(performance.now()-e<d)return;const o=n.target.closest("a");if(!h(o))return;o.addEventListener("mouseout",p,{passive:!0}),t=setTimeout(()=>{v(o.href),t=void 0},l)},n),c&&document.addEventListener("mousedown",function(t){if(performance.now()-e<d)return;const n=t.target.closest("a");if(t.which>1||t.metaKey||t.ctrlKey)return;if(!n)return;n.addEventListener("click",function(t){1337!=t.detail&&t.preventDefault()},{capture:!0,passive:!1,once:!0});const o=new MouseEvent("click",{view:window,bubbles:!0,cancelable:!1,detail:1337});n.dispatchEvent(o)},n),m){let t;(t=window.requestIdleCallback?t=>{requestIdleCallback(t,{timeout:1500})}:t=>{t()})(()=>{const t=new IntersectionObserver(e=>{e.forEach(e=>{if(e.isIntersecting){const n=e.target;t.unobserve(n),v(n.href)}})});document.querySelectorAll("a").forEach(e=>{h(e)&&t.observe(e)})})}}function p(e){e.relatedTarget&&e.target.closest("a")==e.relatedTarget.closest("a")||t&&(clearTimeout(t),t=void 0)}function h(t){if(t&&t.href&&(!r||"instant"in t.dataset)&&(a||t.origin==location.origin||"instant"in t.dataset)&&["http:","https:"].includes(t.protocol)&&("http:"!=t.protocol||"https:"!=location.protocol)&&(s||!t.search||"instant"in t.dataset)&&!(t.hash&&t.pathname+t.search==location.pathname+location.search||"noInstant"in t.dataset))return!0}function v(t){if(n.has(t))return;const e=document.createElement("link");console.log("Prefetched: "+t);e.rel="prefetch",e.href=t,document.head.appendChild(e),n.add(t)};
 
-/* Broken Anchor Checking */
-/* If a reader loads a page and the anchor ID/hash does not exist inside the page, fire off a request to the 404 page, whose logs are reviewed manually, with the offending page+anchor ID, for correction (either fixing an outdated link somewhere on gwern.net, or adding a span/div manually to the page to make old inbound links go where they ought to). */
-/* Such broken anchors can reflect out of date cross-page references, or reflect incoming URLs from elsewhere on the Internet which are broken/outdated. (Within-page anchor links are checked statically at compile-time, and those errors should never exist.) */
-if (location.hash != "") {
- if (null == document.getElementById(location.hash.split('#')[1]))
-  {  brokenHashLog = new URL("https://" + location.hostname + "/static/404.html" +
-                                                        "-error-" +
-                                                        fixedEncodeURIComponent(location.pathname) +
-                                                        "--" +
-                                                        fixedEncodeURIComponent(location.hash.split('#')[1]));
-     doAjax({ location: brokenHashLog,  onSuccess: (e) => {}, onFailure: (e) => {} });
-     console.log("Reporting broken hash-anchor: " + brokenHashLog);
-     }
-     }
-/* Loop over internal page self-links and check that their targets exist; if they do not, report it via 404 like the broken anchors. */
-selfLinks = document.querySelectorAll(".markdownBody a[href^='#']");
-selfLinks.forEach(function(anchor) {
-    anchorExists = document.getElementById(anchor.hash.split('#')[1]);
-    if (null == anchorExists) {
-     brokenHashLog = new URL("https://" + location.hostname + "/static/404.html" +
-                                                        "-error-" +
-                                                        fixedEncodeURIComponent(anchor.pathname) +
-                                                        "--" +
-                                                        fixedEncodeURIComponent(anchor.hash.split('#')[1]));
-     doAjax({ location: brokenHashLog,  onSuccess: (e) => {}, onFailure: (e) => {} });
-     console.log("Reporting broken hash-anchor: " + brokenHashLog);
-    }
-});
