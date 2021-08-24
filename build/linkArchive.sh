e8e21cf6c0acb7f9cde7850df26baa94ccc4e92f
gwern <gwern@gwern.net>
2021-08-23 21:28:25 -0400

    lA.sh: fix conditionals: wasn't erroring out when URL clearly indicated a PDF but the broken source host returned HTML crap

--- build/linkArchive.sh
+++ build/linkArchive.sh
@@ -3,7 +3,7 @@
 # linkArchive.sh: archive a URL through SingleFile and link locally
 # Author: Gwern Branwen
 # Date: 2020-02-07
-# When:  Time-stamp: "2021-04-14 12:55:40 gwern"
+# When:  Time-stamp: "2021-08-23 21:27:33 gwern"
 # License: CC-0
 #
 # Shell script to archive URLs/PDFs via SingleFile for use with LinkArchive.hs:
@@ -19,12 +19,12 @@
 # $ linkArchive.sh 'http://www.jacurutu.com/viewtopic.php?p=101694#p101694'
 # /home/gwern/wiki/docs/www/www.jacurutu.com/718b0de585ef3dcd778a196fb2b8c842b42c7bc2.html
 #
-# Requires: sha1sum, SingleFile+chromium, timeout, curl, wget, ocrmypdf
+# Requires: sha1sum, SingleFile+chromium, timeout, curl, wget, ocrmypdf; pdftk recommended for 'decrypting' PDFs
 
 # set -e
 # set -x
 
-USER_AGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
+USER_AGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
 
 TARGET=""
 ## NOTE: anchor-handling is tricky. We need to drop everything after '#' because it's technically not part of the
@@ -53,8 +53,8 @@ else
         MIME_REMOTE=$(timeout 20s curl --user-agent "$USER_AGENT" \
                           --write-out '%{content_type}' --silent -L -o /dev/null "$URL" || echo "Unsuccessful: $1 $HASH" 1>&2 && exit 1)
 
-        if [[ "$URL" =~ .*'.pdf'.* || "$URL" =~ .*'_pdf'.* || "$URL" =~ '#pdf'.* || "$MIME_REMOTE" =~ "application/pdf".* \
-                  || "$MIME_REMOTE" =~ "application/octet-stream".* ]]; then
+        if [[ "$URL" =~ .*'.pdf'.* || "$URL" =~ .*'_pdf'.* || "$URL" =~ '#pdf'.* || "$URL" =~ .*'/pdf/'.* ]];
+        then
 
             timeout --kill-after=120s 120s wget --user-agent="$USER_AGENT" \
                     --quiet --output-file=/dev/null "$1" --output-document=/tmp/"$HASH".pdf
@@ -63,7 +63,9 @@ else
             ## that we actually got a real PDF file:
             MIME_LOCAL=$(file "$TARGET" | fgrep 'PDF document, version ') || true
 
-            if [[ -f "$TARGET" ]] && [[ -n "$MIME_LOCAL" ]] && [[ ! "$MIME_REMOTE" =~ .*"text/html".* ]] ; then
+            if [[ -f "$TARGET" ]] && [[ -n "$MIME_LOCAL" ]] && [[ ! "$MIME_REMOTE" =~ .*"text/html".* ]] || \
+                   [[ "$MIME_REMOTE" =~ "application/pdf".*  || "$MIME_REMOTE" =~ "application/octet-stream".* ]];
+            then
                 mkdir --parents "./docs/www/$DOMAIN/"
                 ## move the PDF into the Gwern.net repo:
                 mv "$TARGET" "./docs/www/$DOMAIN/$HASH.pdf"
@@ -82,7 +84,7 @@ else
             # apparently you're supposed to `--browser-executable-path` workaround that, which is documented only in a bug report
             timeout --kill-after=240s 240s \
                     ~/src/SingleFile/cli/single-file --browser-executable-path "$(command -v chromium-browser)" --compress-CSS --remove-scripts false \
-                    --browser-extensions $(find ~/.config/chromium/Default/Extensions/* -maxdepth 0 -type d) \
+                    --browser-extensions "$(find ~/.config/chromium/Default/Extensions/* -maxdepth 0 -type d)" \
                     --user-agent "$USER_AGENT" \
                     --browser-load-max-time "240000" \
                     --load-deferred-images-max-idle-time "20000" \
