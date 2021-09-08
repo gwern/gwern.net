7716c7e7480b20b364e667db2ceb0e42cef42388
gwern <gwern@gwern.net>
2021-09-07 23:38:33 -0400

    gD: switch enumeration from list to sections to get a ToC, per shawwn

--- build/generateDirectory.hs
+++ build/generateDirectory.hs
@@ -12,14 +12,14 @@ import System.Directory (listDirectory, doesFileExist, doesDirectoryExist, renam
 import System.Environment (getArgs)
 import System.FilePath (takeDirectory, takeFileName)
 import Text.Pandoc (def, nullAttr, nullMeta, pandocExtensions, runPure, writeMarkdown, writerExtensions,
-                    Block(BulletList, Header, Para, RawBlock), Format(Format), Inline(Code, Link, Space, Span, Str), Pandoc(Pandoc))
+                    Block(BulletList, Header, Para, RawBlock), Format(Format), Inline(Code, Link, Space, Span, Str, RawInline),  Pandoc(Pandoc))
 import qualified Data.Map as M (keys, lookup, size, toList, filterWithKey)
-import qualified Data.Text as T (unpack, pack)
+import qualified Data.Text as T (unpack, pack, append)
 import System.IO (stderr, hPrint)
 import System.IO.Temp (writeSystemTempFile)
 import Control.Monad.Parallel as Par (mapM_)
 
-import LinkMetadata (readLinkMetadata, generateAnnotationBlock, getBackLink, Metadata, MetadataItem)
+import LinkMetadata (readLinkMetadata, generateAnnotationBlock, getBackLink, generateID, Metadata, MetadataItem)
 
 main :: IO ()
 main = do dirs <- getArgs
@@ -35,32 +35,40 @@ generateDirectory mta dir'' = do
   let direntries' = map (\entry -> "/"++dir''++entry) direntries
 
   dirs   <- listDirectories direntries'
+
   pairs  <- listFiles  mta  direntries'
   tagged <- listTagged mta  (init dir'')
-
-  -- descending date order
-  let links = reverse $ sortByDate $ pairs++tagged
+  let links = reverse $ sortByDate $ pairs++tagged -- newest first, to show recent additions
 
   -- remove the tag for *this* directory; it is redundant to display 'cat/catnip' on every doc/link inside '/docs/cat/catnip/index.page', after all.
   let tagSelf = init $ replace "docs/" "" dir'' -- "docs/cat/catnip/" → 'cat/catnip'
   let links' = map (\(y,(a,b,c,d,tags,f),z) -> (y,(a,b,c,d, filter (/= tagSelf) tags,f),z)) links
-  let allUnannotatedFilesP = all (=="") $ map (\(_,(_,_,_,_,_,annotation),_) -> annotation) links'
+
+  -- a very long List can be hard to browse, and doesn't provide a useful ToC. If we have titles, we can use those as section headers.
+  -- (Entries without even a title must be squashed into a list and chucked at the end.)
+  let titledLinks   = filter (\(_,(t,_,_,_,_,_),_) -> t /= "") links'
+  let untitledLinks = filter (\(_,(t,_,_,_,_,_),_) -> t == "") links'
+  let allUnannotatedUntitledP = all (=="") $ map (\(_,(_,_,_,_,_,annotation),_) -> annotation) untitledLinks -- whether to be compact columns
+
+  let titledLinksSections   = generateSections titledLinks
+  let untitledLinksSection  = generateListItems untitledLinks
 
   let header = generateYAMLHeader dir''
   let directorySection = generateDirectoryItems dirs
 
-  let linkSection = generateListItems links'
-  let body = [Header 2 nullAttr [Str "Directories"]] ++
+  let body = [Header 1 nullAttr [Str "Directories"]] ++
                -- for pages like ./docs/statistics/index.page where there are 9+ subdirectories, we'd like to multi-column the directory section (we can't for files/links because there are so many annotations):
                (if length dirs < 8 then [directorySection] else
                  [RawBlock (Format "html") "<div class=\"columns\">\n\n",
                    directorySection,
                    RawBlock (Format "html") "</div>"]) ++
-               [Header 2 nullAttr [Str "Links"]] ++
+               [Header 1 nullAttr [Str "Links"]] ++
+                titledLinksSections ++
+               [Header 1 nullAttr [Str "Miscellaneous"]] ++
                -- for lists, they *may* all be devoid of annotations and short
-                if not allUnannotatedFilesP then [linkSection] else
+                if not allUnannotatedUntitledP then [untitledLinksSection] else
                   [RawBlock (Format "html") "<div class=\"columns\">\n\n",
-                   linkSection,
+                   untitledLinksSection,
                    RawBlock (Format "html") "</div>"]
 
   let document = Pandoc nullMeta body
@@ -131,7 +139,6 @@ listTagged m dir = if not ("docs/" `isPrefixOf` dir) then return [] else -- M.em
 sortByDate :: [(FilePath,MetadataItem,FilePath)] -> [(FilePath,MetadataItem,FilePath)]
 sortByDate = sortBy (\(f,(_,_,d,_,_,_),_) (f',(_,_,d',_,_,_),_) -> if not (null d && null d') then (if d > d' then GT else LT) else (if f > f' then GT else LT))
 
-
 -- how do we handle files with appended data, which are linked like '/docs/reinforcement-learning/2020-bellemare.pdf#google' but exist as files as '/docs/reinforcement-learning/2020-bellemare.pdf'? We can't just look up the *filename* because it's missing the # fragment, and the annotation is usually for the full path including the fragment. If a lookup fails, we fallback to looking for any annotation with the file as a *prefix*, and accept the first match.
 lookupFallback :: Metadata -> String -> (FilePath, MetadataItem)
 lookupFallback m u = case M.lookup u m of
@@ -158,16 +165,24 @@ generateDirectoryItems ds = BulletList
        generateDirectoryItem d = [Para [Link nullAttr [Code nullAttr (T.pack $ "↓ " ++ takeDirectory d)] (T.pack d, "")]]
 
 generateListItems :: [(FilePath, MetadataItem,FilePath)] -> Block
-generateListItems p = BulletList (map generateListItem p)
-generateListItem :: (FilePath,MetadataItem,FilePath) -> [Block]
-generateListItem (f,(t,aut,_,_,_,""),bl)  = let f' = if "http"`isPrefixOf`f then f else if "index" `isSuffixOf` f then takeDirectory f else takeFileName f in
+generateListItems p = BulletList (map generateItem p)
+
+generateSections :: [(FilePath, MetadataItem,FilePath)] -> [Block]
+generateSections = concatMap (\p@(f,(t,aut,dt,_,_,_),_) ->
+                                let sectionID = if aut=="" then "" else (generateID f aut dt) `T.append` "-section" in
+                                 [Header 2 (sectionID, [], []) [RawInline (Format "html") (T.pack $ "“"++t++"”")]]
+                                 ++ generateItem p)
+
+
+generateItem :: (FilePath,MetadataItem,FilePath) -> [Block]
+generateItem (f,(t,aut,_,_,_,""),bl)  = let f' = if "http"`isPrefixOf`f then f else if "index" `isSuffixOf` f then takeDirectory f else takeFileName f in
                                             let author = if aut=="" then [] else [Str ",", Space, Str (T.pack aut)] in
                                               -- I skip date because files don't usually have anything better than year, and that's already encoded in the filename which is shown
                                           let backlink = if bl=="" then [] else [Space, Str "(",  Span ("", ["backlinks"], []) [Link ("",["backlink"],[]) [Str "backlinks"] (T.pack bl,"Reverse citations/backlinks for this page (the list of other pages which link to this URL).")], Str ")"] in
                                             if t=="" then [Para (Link nullAttr [Code nullAttr (T.pack f')] (T.pack f, "") : (author ++ backlink))]
                                             else [Para (Code nullAttr (T.pack f') : (Link nullAttr [Str ":", Space, Str "“", Str (T.pack t), Str "”"] (T.pack f, "")) : (author ++ backlink))]
 
-generateListItem (f,a,bl) =
+generateItem (f,a,bl) =
   -- render annotation as: (skipping DOIs)
   --
   -- > [`2010-lucretius-dererumnatura.pdf`: "On The Nature of Things"](/docs/philosophy/2010-lucretius-dererumnatura.pdf), Lucretius (55BC-01-01):
