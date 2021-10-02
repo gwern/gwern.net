911891ca372c65df03676eba9b4b0baa6688bdb7
gwern <gwern@gwern.net>
2021-10-02 18:20:31 -0400

    +

--- build/LinkAuto.hs
+++ build/LinkAuto.hs
@@ -4,7 +4,7 @@ module LinkAuto (linkAuto) where
 {- LinkAuto.hs: search a Pandoc document for pre-defined regexp patterns, and turn matching text into a hyperlink.
 Author: Gwern Branwen
 Date: 2021-06-23
-When:  Time-stamp: "2021-09-28 19:12:58 gwern"
+When:  Time-stamp: "2021-10-02 09:31:26 gwern"
 License: CC-0
 
 This is useful for automatically defining concepts, terms, and proper names using a single master updated list of regexp/URL pairs.
@@ -1347,4 +1347,5 @@ custom = sortBy (\a b -> compare (T.length $ fst b) (T.length $ fst a)) [
         , ("[Dd]ata[ -][Aa]ugment(ation.?)?", "https://en.wikipedia.org/wiki/Data_augmentation")
         , ("(JFT-300[Mm]?|JFT)", "https://arxiv.org/abs/1707.02968#google")
         , ("JFT-3[Bb]", "https://arxiv.org/abs/2106.04560#google")
+        , ("([Mm]ethylphenidate|Ritalin|Concerta)", "https://en.wikipedia.org/wiki/Methylphenidate")
         ]
