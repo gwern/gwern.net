e3fd950827ea9a431f0ca9929a96fed2545ddebf
gwern <gwern@gwern.net>
2021-10-08 20:25:26 -0400

    addTag: just go whole hog with making the arguments flexible

--- build/addTag.hs
+++ build/addTag.hs
@@ -1,17 +1,27 @@
 #!/usr/bin/env runhaskell
 
 -- CLI tool to add link-tags to specified URLs/paths. eg 'addTag.hs "https://en.wikipedia.org/wiki/Experience_curve_effects" "economics/experience-curve" '
+-- This supports multiple mixed arguments; if there are multiple links and/or multiple tags specified (as identified by links starting with '/' or 'http', and tags not starting with those & also the necessary tag-directory existing on disk), then we add all tags to all links (order irrelevant).
+-- eg 'addTag.hs "https://en.wikipedia.org/wiki/Experience_curve_effects" "economics/experience-curve" "genetics/heritable" "https://www.genome.gov/about-genomics/fact-sheets/DNA-Sequencing-Costs-Data"' would tag the 2 links into 2 tag-directories.
 module Main where
 
-import Control.Monad (when)
+import Control.Monad (filterM, when)
 import Data.List.Utils (replace)
+import Data.List (isPrefixOf)
 import Data.Maybe (isJust, fromJust)
 import System.Environment (getArgs)
-import System.Directory (doesFileExist)
+import System.Directory (doesDirectoryExist, doesFileExist)
+
 import LinkMetadata (annotateLink, readLinkMetadata, readYaml, writeYaml, MetadataList, MetadataItem)
 
 main :: IO ()
-main = do [link, tag] <- getArgs
+main = do args <- getArgs
+          let links = filter (\arg -> head arg == '/' || "http" `isPrefixOf` arg) args
+          tags <- filterM (\arg' -> doesDirectoryExist ("docs/"++arg')) $ filter (\arg -> (not (arg `elem` links))) args
+          mapM_ (\link -> mapM_ (addOneTag link) tags) links
+
+addOneTag :: String -> String -> IO ()
+addOneTag link tag = do
           let link' = replace "https://www.gwern.net/" "/" link
           -- allow shortcut additions like 'addTag.hs docs/foo.pdf psychology'
           link'' <- if not (head link' /= '/' && take 4 link' /= "http") then return link' else
@@ -30,14 +40,14 @@ addAndWriteTags t i c p a  = do let cP = hasItem i c
                                 if cP then writeYaml "metadata/custom.yaml" (addTag i c t) else
                                   if pP then writeYaml "metadata/partial.yaml" (addTag i p t) else
                                     if aP then let (autoNew,partialNew) = mvItem a p i in writeYaml "metadata/auto.yaml" autoNew >> writeYaml "metadata/partial.yaml" (addTag i partialNew t)
-                                    else addNewLink i
+                                    else addNewLink t i
 
 -- what if a link is completely new and is not in either custom.yaml (handwritten) or auto.yaml (often auto-annotated)? If we write it directly into partial.yaml, then for many links like Arxiv/Biorxiv, we'd skip creating an automatic annotation!
 -- So instead we hook back into the main link annotation workflow, create a new annotation for that (which will be in auto.yaml), and then run addTag.hs *again*, so this time it has an annotation to work with (and will do auto.yaml → partial.yaml).
-addNewLink :: String -> IO ()
-addNewLink p = do md <- readLinkMetadata
-                  _ <- annotateLink md p
-                  main
+addNewLink :: String -> String -> IO ()
+addNewLink tag p = do md <- readLinkMetadata
+                      _ <- annotateLink md p
+                      addOneTag p tag
 
 addTag :: String -> MetadataList -> String -> MetadataList
 addTag i ml tag = map (\(path,item@(a,b,c,d,e,f)) -> if i /= path || (tag `elem` e) then (path,item) else
