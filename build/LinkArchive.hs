09e123271cf9647621b141a87a77192e5001202e
gwern <gwern@gwern.net>
2021-12-19 20:56:19 -0500

    +

--- build/LinkArchive.hs
+++ build/LinkArchive.hs
@@ -1,7 +1,7 @@
 {- LinkArchive.hs: module for generating Pandoc external links which are rewritten to a local static mirror which cannot break or linkrot—if something's worth linking, it's worth hosting!
 Author: Gwern Branwen
 Date: 2019-11-20
-When:  Time-stamp: "2021-12-15 17:41:39 gwern"
+When:  Time-stamp: "2021-12-17 23:05:30 gwern"
 License: CC-0
 Dependencies: pandoc, filestore, tld, pretty; runtime: SingleFile CLI extension, Chromium, wget, etc (see `linkArchive.sh`)
 -}
@@ -927,5 +927,6 @@ whiteList url
       , "https://absa.org/" -- homepage
       , "https://www.agriapet.co.uk/" -- homepage
       , "www.word.golf" -- interactive game
+      , "https://demo.allennlp.org" -- interactive demos
       ] = True
     | otherwise = False
