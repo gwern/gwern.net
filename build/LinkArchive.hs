6b17ade39730d7a1ebd4441359359381d2ee9c22
gwern <gwern@gwern.net>
2022-01-04 22:47:35 -0500

    +

--- build/LinkArchive.hs
+++ build/LinkArchive.hs
@@ -1,7 +1,7 @@
 {- LinkArchive.hs: module for generating Pandoc external links which are rewritten to a local static mirror which cannot break or linkrot—if something's worth linking, it's worth hosting!
 Author: Gwern Branwen
 Date: 2019-11-20
-When:  Time-stamp: "2022-01-02 23:34:49 gwern"
+When:  Time-stamp: "2022-01-04 19:33:45 gwern"
 License: CC-0
 Dependencies: pandoc, filestore, tld, pretty; runtime: SingleFile CLI extension, Chromium, wget, etc (see `linkArchive.sh`)
 -}
@@ -145,11 +145,11 @@ archiveURL l = do (exit,stderr',stdout) <- runShellCommand "./" Nothing "linkArc
 -- 3. after that, we may want to skip various filetypes and domains
 whiteList :: String -> Bool
 whiteList url
+  | any (`isInfixOf` url) ["citeseerx.ist.psu.edu"] = True -- TODO: after fixing all existing Citeseer links, set this rule to False
   | any (`isPrefixOf` url) ["/", "./", "../", "https://www.gwern.net", "#", "!", "$", "mailto", "irc", "/metadata/", "/docs/"] = True
   | any (`isSuffixOf` url) [".pdf"] = False
   | any (`isSuffixOf` url) ["/pdf"] = False
   | any (`isInfixOf` url) [".pdf#"] = False
-  | any (`isInfixOf` url) ["citeseerx.ist.psu.edu"] = False
   | any (`isInfixOf` url) [".txt" -- TODO: generalize the PDF download to handle all non-HTML filetypes
       , ".xlsx"
       , ".xz"
@@ -945,5 +945,6 @@ whiteList url
       , "linusakesson.net" -- low quality (YT embed breaks)
       , "https://willcrichton.net/nota/" -- interactive
       , "http://trixter.oldskool.org/" -- low quality (YT embed breaks)
+      , "http://www.michaelburge.us/2019/05/21/marai-agent.html" -- low quality (YT embed breaks)
       ] = True
     | otherwise = False
