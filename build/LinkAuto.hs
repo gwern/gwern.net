8850ed364103acb808b0b81f047f7a92956f9313
gwern <gwern@gwern.net>
2022-01-26 12:28:31 -0500

    LA: docs: hello Raw*ness, my old friend

--- build/LinkAuto.hs
+++ build/LinkAuto.hs
@@ -4,7 +4,7 @@ module LinkAuto (linkAuto, linkAutoFiltered) where
 {- LinkAuto.hs: search a Pandoc document for pre-defined regexp patterns, and turn matching text into a hyperlink.
 Author: Gwern Branwen
 Date: 2021-06-23
-When:  Time-stamp: "2022-01-25 11:37:39 gwern"
+When:  Time-stamp: "2022-01-26 12:17:33 gwern"
 License: CC-0
 
 This is useful for automatically defining concepts, terms, and proper names using a single master updated list of regexp/URL pairs.
@@ -26,7 +26,7 @@ After the regexp pass, we do an additional cleanup pass. How should we handle th
 For the cleanup pass, we track 'seen' `link-auto` links in a Set, and if a link has been seen before, we remove it, leaving the text annotated with a simple Span 'link-auto-skipped' class.
 (In the future, we may drop this clean up pass, if we can find a good way to dynamically hide 'excess' links; one idea is define `.link-auto` CSS to de-style links, and then, on browser screen scroll, use JS to re-link-style the first instance of each URL. So only the first instance would be visible on each screen, minimizing redundancy/clutter/over-linking.)
 
-Bugs: will annotate phrases inside `Header` nodes, which breaks HTML validation.
+Bugs: will annotate phrases inside `Header` nodes, which breaks HTML validation. Does not attempt to handle `RawInline` or `RawBlock`, so writing raw HTML like `<a href="/Modafinil">foo</a>` will not be detected for the purposes of rewrite-short-circuiting or possibly rewriting at all.
 
 Dependencies: Pandoc, text, regex-tdfa, /static/build/Utils.hs, /static/build/Query.hs
 -}
@@ -151,9 +151,9 @@ mergeSpaces (x:xs)                 = x:mergeSpaces xs
 -- Optimization: take a set of definitions, and a document; query document for existing URLs; if a URL is already present, drop it from the definition list.
 -- This avoids redundancy with links added by hand or other filters.
 --
--- NOTE: This can be used to disable link rewrites by manually adding a link. In cases of self-links (eg /Modafinil will contain the word 'modafinil' and get a rewrite to /Modafinil, leading to a useless self-link), it is easier to add a link to disable the rewrite than to figure out how to filter out that one exact rewrite only on that page. This link can be hidden to avoid distracting the reader.
+-- NOTE: This can be used to disable link rewrites by manually adding a link. In cases of self-links (eg. /Modafinil will contain the word 'modafinil' and get a rewrite to /Modafinil, leading to a useless self-link), it is easier to add a link to disable the rewrite than to figure out how to filter out that one exact rewrite only on that page. This link can be hidden to avoid distracting the reader.
 -- So to disable the modafinil rewrite on /Modafinil, one could insert into the Markdown a line like:
--- `<span style="display:none;"><a href="/Modafinil">null</a></span> <!-- LinkAuto override: disable self-linking -->`
+-- `<span style="display:none;">[null](/Modafinil)</span> <!-- LinkAuto override: disable self-linking -->`
 filterDefinitions :: Pandoc -> [(T.Text, R.Regex, T.Text)] -> [(T.Text, R.Regex, T.Text)]
 filterDefinitions p = let allLinks = S.fromList $ map (T.replace "https://www.gwern.net/" "/") $ extractURLs p in
                                           filter (\(_,_,linkTarget) -> linkTarget `notElem` allLinks)
