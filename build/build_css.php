020f4b72a3c9808e44fd628201fb3a03c12ee1af
Said Achmiz <said@saidachmiz.net>
2022-01-30 11:00:44 +0000

    Slightly reorganized CSS asset directory structure

--- build/build_css.php
+++ build/build_css.php
@@ -3,7 +3,7 @@
 $css_dir = __DIR__ . "/../css";
 $build_dir = __DIR__;
 
-`php {$build_dir}/color-scheme-convert.php {$css_dir}/colors.css 1 "Lab" 0.55 > {$css_dir}/colors-dark.css`;
-`cat {$css_dir}/colors-dark.css {$css_dir}/dark-mode-adjustments.css > {$css_dir}/dark-mode.css`;
+`php {$build_dir}/color-scheme-convert.php {$css_dir}/include/colors.css 1 "Lab" 0.55 > {$css_dir}/include/components/colors-dark-GENERATED.css`;
+`cat {$css_dir}/include/components/colors-dark-GENERATED.css {$css_dir}/include/components/dark-mode-adjustments.css > {$css_dir}/include/dark-mode-GENERATED.css`;
 
 ?>
--- build/pre-commit.hook.php
+++ build/pre-commit.hook.php
@@ -6,12 +6,12 @@ $force = @$argv[1] == "--force";
 $build_dir = __DIR__;
 $static_dir = "{$build_dir}/..";
 
-if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/colors.css {$static_dir}/css/dark-mode-adjustments.css`)) {
+if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/include/colors.css {$static_dir}/css/include/components/dark-mode-adjustments.css`)) {
 	require_once("{$build_dir}/build_css.php");
 	`git add {$static_dir}/css/.`;
 }
 
-if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/colors.css {$static_dir}/css/initial.css {$static_dir}/css/dark-mode.css {$static_dir}/js/gw-inline.js {$static_dir}/js/darkmode-inline.js`)) {
+if ($force || (`git diff-index --cached HEAD -- {$static_dir}/css/include/colors.css {$static_dir}/css/include/initial.css {$static_dir}/css/include/dark-mode-GENERATED.css {$static_dir}/js/gw-inline.js {$static_dir}/js/darkmode-inline.js`)) {
 	require_once("{$build_dir}/build_head_includes.php");
 	`git add {$static_dir}/includes/.`;
 }
