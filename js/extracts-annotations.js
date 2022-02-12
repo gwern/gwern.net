f04d7b121e8fcddc4e76383395f96bb67ee1f4a6
gwern <gwern@gwern.net>
2022-02-11 20:47:55 -0500

    extracts: smallcaps the [HTML]

--- js/extracts-annotations.js
+++ js/extracts-annotations.js
@@ -77,7 +77,7 @@ if (window.Extracts) {
         let originalLinkHTML = "";
         if (   referenceData.element.dataset.urlOriginal != undefined
                && referenceData.element.dataset.urlOriginal != target.href) {
-            let originalURLText = referenceData.element.dataset.urlOriginal.includes("ar5iv") ? "HTML" : "live";
+            let originalURLText = referenceData.element.dataset.urlOriginal.includes("ar5iv") ? `<span class="smallcaps">HTML</span>` : "live";
             originalLinkHTML = `<span class="originalURL">[<a
                             title="Link to original URL for ${referenceData.element.textContent}"
                             href="${referenceData.element.dataset.urlOriginal}"
