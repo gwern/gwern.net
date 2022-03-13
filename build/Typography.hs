a5630dc762442062454bca35f614595d730b2b87
gwern <gwern@gwern.net>
2022-03-13 16:07:45 -0400

    CSS: document some confusing points

--- build/Typography.hs
+++ build/Typography.hs
@@ -4,7 +4,7 @@
 -- 1. adding smallcaps to capitalized phrases
 -- 2. adding line-break tags (`<wbr>` as Unicode ZERO WIDTH SPACE) to slashes so web browsers break at slashes in text
 -- 3. Adding classes to horizontal rulers (nth ruler modulo 3, allowing CSS to decorate it in a
---    cycling pattern, like `class="ruler-1"`/`class="ruler-2"`/`class="ruler-3"`/`class="ruler-1"`...,
+--    cycling pattern, like `class="horizontalRule-nth-1"`/`class="horizontalRule-nth-2"`/`class="horizontalRule-nth-3"`/`class="horizontalRule-nth-1"`...,
 --    like a repeating pattern of stars/moon/sun/stars/moon/sun... CSS can do this with :nth, but only
 --    for immediate sub-children, it can't count elements *globally*, and since Pandoc nests horizontal
 --    rulers and other block elements within each section, it is not possible to do the usual trick
