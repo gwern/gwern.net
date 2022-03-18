ad0810d22ecba2a544ab752322610721b06f80cc
Said Achmiz <said@saidachmiz.net>
2022-03-18 05:40:03 +0000

    Added toggle UI to reader-mode.js

--- js/reader-mode.js
+++ js/reader-mode.js
@@ -20,6 +20,11 @@ ReaderMode = {
 
 	maskedLinksKeyToggleInfoAlert: null,
 
+	linkMaskingActive: false,
+
+	readerModeToggleButton: null,
+	readerModeToggleButtonInteractable: true,
+
 	state: {
 		hoveringOverLink: false,
 		altKeyPressed: false
@@ -32,50 +37,11 @@ ReaderMode = {
 	/*	Enables reader mode.
 	 */
 	setup: () => {
-		ReaderMode.maskLinks();
-		ReaderMode.updateVisibility({ maskedLinksVisible: false, maskedLinksKeyToggleInfoAlertVisible: false });
-	},
+		GWLog("ReaderMode.setup", "reader-mode.js", 1);
 
-	/*	Masks links, as appropriate. This will hide linkicons and pop-frame 
-		indicators, and will thus cause reflow.
-	 */
-	maskLinks: () => {
-		/*	Get a list of all the links that are to be masked.
-		 */
-		ReaderMode.maskedLinks = ReaderMode.markdownBody.querySelectorAll(ReaderMode.maskedLinksSelector);
+		ReaderMode.injectReaderModeToggleButton();
 
-		/*	Mask links.
-		 */
-		ReaderMode.maskedLinks.forEach(link => {
-			/*	Annotate each link with a class.
-			 */
-			link.classList.add("masked-link");
-
-			/*	Insert hooks for linkicons.
-			 */
-			link.insertAdjacentHTML("beforeend", `<span class='icon-hook'><span></span></span>`);
-
-			/*	Add `mouseenter` / `mouseleave` listeners to show/hide masked 
-				links on hover.
-			 */
-			link.removeMouseEnterEvent = onEventAfterDelayDo(link, "mouseenter", ReaderMode.showMaskedLinksDelay, ReaderMode.updateState, "mouseleave");
-			link.removeMouseLeaveEvent = onEventAfterDelayDo(link, "mouseleave", 0, ReaderMode.updateState);
-
-			/*	Add custom popup trigger delay.
-			 */
-			link.specialPopupTriggerDelay = () => {
-				return (ReaderMode.maskedLinksVisible() == false 
-						? ReaderMode.adjustedPopupTriggerDelay 
-						: Popups.popupTriggerDelay);
-			};
-
-			/*	Add custom link click behavior.
-			 */
-			link.onclick = (event) => { return (ReaderMode.maskedLinksVisible() == true); };
-		});
-
-		/*	Inject style block.
-		 */
+		//	Inject style block.
 		document.querySelector("head").insertAdjacentHTML("beforeend", `<style id='masked-links-styles'>
 			.markdownBody .masked-link .indicator-hook {
 				padding-left: 0;
@@ -87,7 +53,7 @@ ReaderMode = {
 					-0.17em -0.05em 0 0 var(--GW-link-underline-background-color),
 					-0.17em 0 0 0 var(--GW-link-underline-background-color);
 			}
-			.markdownBody .masked-link.masked-links-hidden .indicator-hook,
+			.markdownBody.masked-links-hidden .masked-link .indicator-hook,
 			.markdownBody .masked-link::after {
 				display: none;
 			}
@@ -131,14 +97,187 @@ ReaderMode = {
 					1px 1px 3px 0 #000;
 				font-feature-settings: 'smcp';
 			}
+			#reader-mode-toggle-button {
+				position: fixed;
+				top: calc(0.5em + 3px);
+				right: 12em;
+				z-index: 1;
+				opacity: 0.25;
+				visibility: visible;
+				transition:
+					opacity 2s ease;
+			}
+			@media only screen and (max-width: 1760px) {
+				#reader-mode-toggle-button {
+					top: 7em;
+					right: 0.45em;
+				}
+			}
+			#reader-mode-toggle-button.hidden {
+				opacity: 0;
+			}
+			#reader-mode-toggle-button:hover {
+				opacity: 1.0;
+				transition: none;
+			}
+			#reader-mode-toggle-button button {
+				-moz-appearance: none;
+				appearance: none;
+				border: none;
+				font-family: inherit;
+				font-size: inherit;
+				background: inherit;
+				color: var(--GW-popups-show-popup-options-dialog-button-color);
+				fill: currentColor;
+				opacity: 0.47;
+				padding: 0;
+				line-height: 1;
+				display: block;
+				width: 1.125em;
+			}
+			#reader-mode-toggle-button button:hover {
+				cursor: pointer;
+				opacity: 1.0;
+			}
+			#reader-mode-toggle-button button:active {
+				transform: scale(0.95);
+			}
 		</style>`);
 
-		/*	Add info alert.
-		 */
+		if (ReaderMode.linkMaskingEnabled()) {
+			ReaderMode.maskLinks();
+		}
+	},
+
+	/*	Returns true if link masking is set to be enabled for the current page, 
+		false otherwise.
+	 */
+    linkMaskingEnabled: () => {
+        return true;
+    },
+
+	//	Called by: ReaderMode.setup
+	injectReaderModeToggleButton: () => {
+		GWLog("ReaderMode.injectReaderModeSelector", "reader-mode.js", 1);
+
+		//  Create and inject the button.
+		ReaderMode.readerModeToggleButton = addUIElement(`<div id="reader-mode-toggle-button">` 
+			+ `<button type="button"><img></button>` 
+			+ `</div>`);
+
+		//  Add event listeners and update state.
+		requestAnimationFrame(() => {
+			ReaderMode.updateReaderModeToggleButtonState();
+			ReaderMode.readerModeToggleButton.querySelector("button").addActivateEvent(ReaderMode.readerModeToggleButtonClicked = (event) => {
+				GWLog("ReaderMode.readerModeToggleButtonClicked", "reader-mode.js", 2);
+
+				event.stopPropagation();
+
+				if (ReaderMode.readerModeToggleButtonInteractable == false)
+					return;
+
+				ReaderMode.readerModeToggleButtonInteractable = false;
+
+				if (ReaderMode.linkMaskingActive) {
+					ReaderMode.unmaskLinks();
+				} else {
+					ReaderMode.maskLinks();
+				}
+			
+				requestAnimationFrame(() => {
+					ReaderMode.updateReaderModeToggleButtonState();
+					ReaderMode.readerModeToggleButtonInteractable = true;
+				});
+			});
+			ReaderMode.readerModeToggleButton.addEventListener("mouseenter", () => { ReaderMode.showReaderModeToggleButton(); });
+		});
+
+		//	Show/hide the button on scroll up/down.
+		addScrollListener(ReaderMode.updateReaderModeToggleButtonVisibility, 
+			"ReaderMode.updateReaderModeToggleButtonVisibilityScrollListener");
+	},
+
+	//	Called by: ReaderMode.injectReaderModeToggleButton
+	//	Called by: ReaderMode.readerModeToggleButtonClicked
+	updateReaderModeToggleButtonState: () => {
+		let button = ReaderMode.readerModeToggleButton.querySelector("button");
+		button.title = ReaderMode.linkMaskingActive
+					   ? "Disable reader mode"
+					   : "Enable reader mode";
+
+		let buttonImage = button.firstElementChild;
+		buttonImage.src = ReaderMode.linkMaskingActive
+						  ? "/static/img/icons/book-open-solid.svg"
+						  : "/static/img/icons/book-open.svg";
+	},
+
+	//	Called by: ReaderMode.updateReaderModeToggleButtonVisibilityScrollListener
+	updateReaderModeToggleButtonVisibility: () => {
+		GWLog("ReaderMode.updateReaderModeToggleButtonVisibility", "reader-mode.js", 3);
+
+		if (ReaderMode.readerModeToggleButton == null)
+			return;
+
+		// Hide button when scrolling a full page down.
+		if (GW.scrollState.unbrokenDownScrollDistance > window.innerHeight)
+			ReaderMode.hideReaderModeToggleButton();
+
+		// Show back-to-top link on ANY scroll up.
+		if (GW.scrollState.unbrokenUpScrollDistance > window.innerHeight || GW.scrollState.lastScrollTop <= 0)
+			ReaderMode.showReaderModeToggleButton();
+	},
+
+	//	Called by: ReaderMode.updateReaderModeToggleButtonVisibility
+	//	Called by: reader mode toggle button ‘mouseenter’ event handler
+	showReaderModeToggleButton: () => {
+		ReaderMode.readerModeToggleButton.classList.remove("hidden");
+	},
+
+	//	Called by: ReaderMode.updateReaderModeToggleButtonVisibility
+	hideReaderModeToggleButton: () => {
+		ReaderMode.readerModeToggleButton.classList.add("hidden");
+	},
+
+	/*	Masks links, as appropriate. This will hide linkicons and pop-frame 
+		indicators, and will thus cause reflow.
+	 */
+	maskLinks: () => {
+		GWLog("ReaderMode.maskLinks", "reader-mode.js", 1);
+
+		ReaderMode.linkMaskingActive = true;
+
+		//	Get a list of all the links that are to be masked.
+		ReaderMode.maskedLinks = ReaderMode.markdownBody.querySelectorAll(ReaderMode.maskedLinksSelector);
+
+		//	Mask links.
+		ReaderMode.maskedLinks.forEach(link => {
+			//	Annotate each link with a class.
+			link.classList.add("masked-link");
+
+			//	Insert hooks for linkicons.
+			link.insertAdjacentHTML("beforeend", `<span class='icon-hook'><span></span></span>`);
+
+			/*	Add `mouseenter` / `mouseleave` listeners to show/hide masked 
+				links on hover.
+			 */
+			link.removeMouseEnterEvent = onEventAfterDelayDo(link, "mouseenter", ReaderMode.showMaskedLinksDelay, ReaderMode.updateState, "mouseleave");
+			link.removeMouseLeaveEvent = onEventAfterDelayDo(link, "mouseleave", 0, ReaderMode.updateState);
+
+			//	Add custom popup trigger delay.
+			link.specialPopupTriggerDelay = () => {
+				return (ReaderMode.maskedLinksVisible() == false 
+						? ReaderMode.adjustedPopupTriggerDelay 
+						: Popups.popupTriggerDelay);
+			};
+
+			//	Add custom link click behavior.
+			link.onclick = (event) => { return (ReaderMode.maskedLinksVisible() == true); };
+		});
+
+		//	Add info alert.
 		ReaderMode.maskedLinksKeyToggleInfoAlert = addUIElement(`<div id='masked-links-key-toggle-info-alert'><p>Hold <span class="key">alt</span> / <span class="key">option</span> key to show links</p></div>`);
 
-		/*	Add key down/up listeners, to show/hide masked links with Alt key.
-		 */
+		//	Add key down/up listeners, to show/hide masked links with Alt key.
 		document.addEventListener("keydown", ReaderMode.altKeyDownOrUp = (event) => {
 			if (event.key != "Alt")
 				return;
@@ -160,20 +299,23 @@ ReaderMode = {
 			});
 		}, { threshold: 1.0 });
 		observer.observe(document.querySelector(ReaderMode.unmaskLinksTriggerElementSelector));
+
+		//	Update visual state.
+		ReaderMode.updateVisibility({ maskedLinksVisible: false, maskedLinksKeyToggleInfoAlertVisible: false });
 	},
 
 	/*	Unmasks links. This will un-hide linkicons and pop-frame indicators, and
 		will thus cause reflow.
 	 */
 	unmaskLinks: () => {
+		GWLog("ReaderMode.unmaskLinks", "reader-mode.js", 1);
+
+		ReaderMode.linkMaskingActive = false;
+
+		//	Show masked links.
 		ReaderMode.showMaskedLinks();
 
-		/*	Remove style block (if present).
-		 */
-		document.querySelectorAll("#masked-links-styles").forEach(styles => { styles.remove() });
-
-		/*	Remove info alert.
-		 */
+		//	Remove info alert.
 		document.querySelectorAll("#masked-links-key-toggle-info-alert").forEach(alert => { alert.remove() });
 
 		/*	Unmask every masked link. (Note that ReaderMode.maskedLinks is a 
@@ -184,28 +326,23 @@ ReaderMode = {
 		ReaderMode.maskedLinks.forEach(link => {
 			link.classList.remove("masked-link");
 
-			/*	Extract hooks.
-			 */
+			//	Extract hooks.
 			link.querySelectorAll(".icon-hook").forEach(hook => { hook.remove() });
 
-			/*	Remove `mouseenter` / `mouseleave` listeners from the link.
-			 */
+			//	Remove `mouseenter` / `mouseleave` listeners from the link.
 			link.removeMouseEnterEvent();
 			link.removeMouseLeaveEvent();
 			link.removeMouseEnterEvent = null;
 			link.removeMouseLeaveEvent = null;
 
-			/*	Remove custom popup trigger delay.
-			 */
+			//	Remove custom popup trigger delay.
 			link.specialPopupTriggerDelay = null;
 
-			/*	Re-enable normal link click behavior.
-			 */
+			//	Re-enable normal link click behavior.
 			link.onclick = null;
 		});
 
-		/*	Remove key down/up listeners (for the Alt key toggle).
-		 */
+		//	Remove key down/up listeners (for the Alt key toggle).
 		document.removeEventListener("keydown", ReaderMode.altKeyDownOrUp);
 		document.removeEventListener("keyup", ReaderMode.altKeyDownOrUp);
 		ReaderMode.altKeyDownOrUp = null;
@@ -221,6 +358,8 @@ ReaderMode = {
 	/*	Hides masked links (if any).
 	 */
 	hideMaskedLinks: () => {
+		GWLog("ReaderMode.hideMaskedLinks", "reader-mode.js", 2);
+
 		/*	Hide masked links. (Because linkicons and pop-frame indicators are
 			already hidden, this causes no reflow).
 		 */
@@ -230,6 +369,8 @@ ReaderMode = {
 	/*	Unhides masked links (if any).
 	 */
 	showMaskedLinks: () => {
+		GWLog("ReaderMode.showMaskedLinks", "reader-mode.js", 2);
+
 		/*	Unhide masked links. (This does not reveal linkicons or pop-frame
 			indicators, and thus causes no reflow).
 		 */
@@ -239,20 +380,25 @@ ReaderMode = {
 	/*	Hide key toggle info alert.
 	 */
 	hideKeyToggleInfoAlert: () => {
+		GWLog("ReaderMode.hideKeyToggleInfoAlert", "reader-mode.js", 2);
+
 		ReaderMode.maskedLinksKeyToggleInfoAlert.classList.add("hidden");
 	},
 
 	/*	Show key toggle info alert.
 	 */
 	showKeyToggleInfoAlert: () => {
+		GWLog("ReaderMode.showKeyToggleInfoAlert", "reader-mode.js", 2);
+
 		ReaderMode.maskedLinksKeyToggleInfoAlert.classList.remove("hidden");
 	},
 
 	/*	Update state after an event that might cause a visibility change.
 	 */
 	updateState: (event) => {
-		/*	Update tracked state.
-		 */
+		GWLog("ReaderMode.updateState", "reader-mode.js", 3);
+
+		//	Update tracked state.
 		switch (event.type) {
 			case 'mouseenter':
 				ReaderMode.state.hoveringOverLink = true;
@@ -276,8 +422,7 @@ ReaderMode = {
 		let shouldShowMaskedLinks = (ReaderMode.state.hoveringOverLink || ReaderMode.state.altKeyPressed);
 		let shouldShowMaskedLinksKeyToggleInfoAlert = (ReaderMode.state.hoveringOverLink && !ReaderMode.state.altKeyPressed);
 
-		/*	Request the desired visibility update.
-		 */
+		//	Request the desired visibility update.
 		ReaderMode.updateVisibility({
 			maskedLinksVisible: shouldShowMaskedLinks,
 			maskedLinksKeyToggleInfoAlertVisible: shouldShowMaskedLinksKeyToggleInfoAlert
@@ -289,6 +434,8 @@ ReaderMode = {
 		toggle info alert panel.)
 	 */
 	updateVisibility: (update) => {
+		GWLog("ReaderMode.updateVisibility", "reader-mode.js", 3);
+
 		/*	Show or hide masked links, depending on what visibility update has
 			been requested, and whether it is necessary (i.e., whether or not
 			things already are as they should be).
@@ -301,8 +448,7 @@ ReaderMode = {
 			ReaderMode.hideMaskedLinks();
 		}
 
-		/*	Likewise, show or hide the key toggle info alert panel, as needed.
-		 */
+		//	Likewise, show or hide the key toggle info alert panel, as needed.
 		if (update.maskedLinksKeyToggleInfoAlertVisible) {
 			ReaderMode.showKeyToggleInfoAlert();
 		} else {
