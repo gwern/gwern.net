ecfee354e1b5dc7e4f6d13ee3978b33bbc18f679
Said Achmiz <said@saidachmiz.net>
2022-04-10 21:34:49 +0000

    Rectify layout of popup options dialog

--- js/extracts-options.js
+++ js/extracts-options.js
@@ -84,27 +84,31 @@ Extracts = { ...Extracts, ...{
 			let enabledRadioButtonChecked = Extracts.popupsEnabled() ? `checked=""` : ``;
 			let disabledRadioButtonChecked = Extracts.popupsEnabled() ? `` : `checked=""`;
 
-			Extracts.popupOptionsDialog = addUIElement(`<div id='popup-options-dialog' style='display: none;'><div>` + 
-				`<h1>Popups</h1>` + 
-				`<form class="option-buttons">
-					<label>
-						<input class="popups-enable" name="popups-enable-status" ${enabledRadioButtonChecked} value="enabled" type="radio">
-						<span class='button-text'>
-							<span class='label'>Enable</span>
-							<span class='explanation'>Show popups when hovering over annotated links.</span>
-						</span>
-					</label>
-					<label>
-						<input class="popups-disable" name="popups-enable-status" ${disabledRadioButtonChecked} value="disabled" type="radio">
-						<span class='button-text'>
-							<span class='label'>Disable</span>
-							<span class='explanation'>Don’t show popups.</span>
-						</span>
-					</label>
-				</form>` +
-				`<button type='button' class='close-button'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg></button>` + 
-				`<button type='button' class='save-button'>Save</button>` + 
-				`</div></div>`);
+			Extracts.popupOptionsDialog = addUIElement(`<div id="popup-options-dialog" style="display: none;"><div>` + 
+				`<div class="popup-options-dialog-title-bar">` + 
+					`<button type="button" class="close-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg></button>` + 
+					`<h1>Popups</h1>` + 
+					`<button type="button" class="save-button">Save</button>` + 
+				`</div>` + 
+				`<div class="controls">` + 
+					`<form class="option-buttons">
+						<label>
+							<input class="popups-enable" name="popups-enable-status" ${enabledRadioButtonChecked} value="enabled" type="radio">
+							<span class="button-text">
+								<span class="label">Enable</span>
+								<span class="explanation">Show popups when hovering over annotated links.</span>
+							</span>
+						</label>
+						<label>
+							<input class="popups-disable" name="popups-enable-status" ${disabledRadioButtonChecked} value="disabled" type="radio">
+							<span class="button-text">
+								<span class="label">Disable</span>
+								<span class="explanation">Don’t show popups.</span>
+							</span>
+						</label>
+					</form>` +
+				`</div>` +
+			`</div></div>`);
 
 			//  Add event listeners.
 			requestAnimationFrame(() => {
