ce107611dabeb936fc2d132ad3f43a22e6e9c043
gwern <gwern@gwern.net>
2022-09-11 19:51:15 -0400

    htmlClassesExtract.py: vectorize to make efficient enough for use; enable HTML class auditing in sync-gwern.net.sh

--- build/htmlClassesExtract.py
+++ build/htmlClassesExtract.py
@@ -4,28 +4,31 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-page = open(sys.argv[1], "r").read()
-
-# parse html content
-soup = BeautifulSoup(page, 'html.parser')
-
-# get all tags
-tags = {tag.name for tag in soup.find_all()}
 
 # class list set
 class_list = set()
 
-# iterate all tags
-for tag in tags:
+for fileIndex in range(1, len(sys.argv)):
 
-    # find all element of tag
-    for i in soup.find_all(tag):
+    page = open(sys.argv[fileIndex], "r").read()
 
-        # if tag has attribute of class
-        if i.has_attr("class"):
+    # parse html content
+    soup = BeautifulSoup(page, 'html.parser')
 
-            if len(i['class']) != 0:
-                class_list.add(" ".join(i['class']))
+    # get all tags
+    tags = {tag.name for tag in soup.find_all()}
+
+    # iterate all tags
+    for tag in tags:
+
+        # find all element of tag
+        for i in soup.find_all(tag):
+
+            # if tag has attribute of class
+            if i.has_attr("class"):
+
+                if len(i['class']) != 0:
+                    class_list.add(" ".join(i['class']))
 
 for val in class_list:
     print(val)
