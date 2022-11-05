f2ba4e2f65ebefe247c7cc5f1d363bbf30fcf1c5
Said Achmiz <said@saidachmiz.net>
2022-11-05 18:21:09 +0000

    More intelligent string processing in typography.js

--- js/typography.js
+++ js/typography.js
@@ -15,6 +15,10 @@
 	MIT License
 	*/
 
+const Typography_pL = "a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ";
+const Typography_word = `[${Typography_pL}_0-9]`;
+const Typography_nonWord = `[^${Typography_pL}_0-9]`;
+
 Typography = {
 	replacements: (types) => {
 		let allReplacements = [ ];
@@ -47,18 +51,17 @@ Typography = {
 	replacementDefinitionGroups: {
 		quotes: [
 			// triple prime
-			[ /'''/g, '\u2034' ],
+			[ /'''/g, retainLength => '\u2034' + (retainLength ? '\u2063\u2063' : '') ],
 			// beginning "
 			[ /([\s([]|^)"([^\s?!.,;\/)])/g, '$1\u201c$2' ],
 			// ending "
-			[ /(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2' ],
+			[ /(\u201c[^"]*)"([^"]*$|[^\u201c"]*(?=\u201c))/g, '$1\u201d$2' ],
 			// remaining " at end of word
 			[ /([^0-9])"/g, '$1\u201d' ],
 			// double quotes
-			// NOTE: This rule cannot be used until the code is de-localized.
-// 			[ /"(.+?)"/g, '\u201c$1\u201d' ],
+			[ /"(.+?)"/g, '\u201c$1\u201d' ],
 			// double prime as two single quotes
-			[ /''/g, '\u2033' ],
+			[ /''/g, retainLength => '\u2033' + (retainLength ? '\u2063' : '') ],
 			// beginning '
 			[ /(\W|^)'(\S)/g, '$1\u2018$2' ],
 			// conjunction's possession
@@ -76,50 +79,62 @@ Typography = {
 		],
 		hyphens: [
 			// turn a hyphen surrounded by spaces, between words, into an em-dash
-			[ /([a-z\u201d]) - ([a-z\u201c])/ig, '$1\u2014$2' ],
+			[ /([a-z\u201d]) - ([a-z\u201c])/ig, retainLength => (retainLength ? '$1\u2063\u2014\u2063$2' : '$1\u2014$2') ],
 			// turn a hyphen between a space and a quote, into an em-dash
-			[ /([a-z]) -(\u201d)/ig, '$1\u2014$2' ],
-			[ /(\u201c)- ([a-z])/ig, '$1\u2014$2' ],
+			[ /([a-z]) -(\u201d)/ig, retainLength => (retainLength ? '$1\u2063\u2014$2' : '$1\u2014$2') ],
+			[ /(\u201c)- ([a-z])/ig, retainLength => (retainLength ? '$1\u2014\u2063$2' : '$1\u2014$2') ],
 			// turn a double or triple hyphen, optionally surrounded by spaces, between words, or at the start of a line, into an em-dash
-			[ /([a-z"'“”‘’]|\n) ?---? ?([a-z"'“”‘’])/ig, '$1\u2014$2' ],
+			[ /([a-z"'“”‘’]|\n)( ?---? ?)([a-z"'“”‘’])/ig, retainLength => (retainLength 
+				? (m0, m1, m2, m3) => (m1 + '\u2014'.padStart(m2.length - 1, '\u2063') + m3)
+				: (m0, m1, m2, m3) => (m1 + '\u2014' + m3)
+			  ) ],
 			// turn a hyphen surrounded by spaces, between decimal digits, into an en-dash
-			[ /([0-9]) - ([0-9])/g, '$1\u2013$2' ]
+			[ /([0-9]) - ([0-9])/g, retainLength => (retainLength ? '$1\u2063\u2013\u2063$2' : '$1\u2013$2') ]
 		],
 		ellipses: [
 			// Ellipsis rectification.
-			[ /(^|\s)\.\.\./g, '$1…' ],
-			[ /\.\.\.(\s|$)/g, '…$1' ]
+			[ /(^|\s)\.\.\./g, retainLength => (retainLength ? '$1…\u2063\u2063' : '$1…') ],
+			[ /\.\.\.(\s|$)/g, retainLength => (retainLength ? '\u2063\u2063…$1' : '…$1') ]
 		],
 		arrows: [
-			// Arrowss
-			[ /(\s)->(\s)/g, '$1\u2192$2' ],
-			[ /(\s)<-(\s)/g, '$1\u2190$2' ],
-			[ /(\s)=>(\s)/g, '$1\u21d2$2' ],
-			[ /(\s)<=(\s)/g, '$1\u21d0$2' ],
-			[ /(\s)<=>(\s)/g, '$1\u21d4$2' ]
+			// Arrows
+			[ /(\s)->(\s)/g, retainLength => (retainLength ? '$1\u2063\u2192$2' : '$1\u2192$2') ],
+			[ /(\s)<-(\s)/g, retainLength => (retainLength ? '$1\u2192\u2063$2' : '$1\u2190$2') ],
+			[ /(\s)=>(\s)/g, retainLength => (retainLength ? '$1\u2063\u2192$2' : '$1\u21d2$2') ],
+			[ /(\s)<=(\s)/g, retainLength => (retainLength ? '$1\u2192\u2063$2' : '$1\u21d0$2') ],
+			[ /(\s)<=>(\s)/g, retainLength => (retainLength ? '$1\u2063\u2192\u2063$2' : '$1\u21d4$2') ]
 		],
+		/*	This replacement type adds length, so the ‘retainLength’ trick does
+			not work. See the ‘processElement’ method for how we deal with this.
+		 */
 		wordbreaks: [
 			// Word-breaks after slashes (for long URLs etc.).
 			[ /.\/+/g, '$&\u200b' ],
 		],
 		misc: [
-			// cConvert nbsp to regular space.
+			// Convert nbsp to regular space.
 			[ /\xa0/g, ' ' ],
 			// Two spaces after a period is INCORRECT.
-			[ /(\w[\.\?\!])[ \u00a0]{2}(\w)/g, '$1 $2' ],
+			[ /(\w[\.\?\!])[ \u00a0]{2}(\w)/g, retainLength => (retainLength ? '$1 \u2063$2' : '$1 $2') ],
 			// Hyphen followed by a numeral (with an optional space first), becomes an actual minus sign.
 			[ /(\s)-( ?)([0-9])/g, '$1\u2212$2$3' ]
 		],
 		softHyphens: [
 			// Strip soft hyphens.
-			[ /\u00ad/g, '' ]
+			[ /\u00ad/g, retainLength => (retainLength ? '\u2063' : '') ]
 		]
 	},
-	processString: (str, replacementTypes = Typography.replacementTypes.ALL) => {
+	processString: (str, replacementTypes = Typography.replacementTypes.ALL, options = { }) => {
 		Typography.replacements(replacementTypes).forEach(replace => {
-			str = str.replace(replace[0], replace[1]);
+			replacement = typeof replace[1] === "function"
+						  ? replace[1](options.retainLength)
+						  : replace[1];
+			str = str.replace(replace[0], replacement);
 		});
 		return str;
+	},
+	substringSansSeparators: (text, value, position) => {
+		return text.substr(position, value.length).replace('\u2063', '');
 	},
 	replaceZeroWidthSpaces: (element) => {
 		let mustReplace = false;
@@ -157,16 +172,43 @@ Typography = {
 		if ([ 'CODE', 'PRE', 'SCRIPT', 'STYLE', 'NOSCRIPT' ].includes(element.nodeName))
 			return;
 
+		//	Word breaks require special treatment, because they add length.
+		let doWordbreaks = (replacementTypes & Typography.replacementTypes.WORDBREAKS);
+		replacementTypes &= ~(Typography.replacementTypes.WORDBREAKS);
+
+		let text = "";
+		let textNodes = [ ];
+
+		//	We process everything *but* the wordbreaks rule in a non-local way.
 		for (node of element.childNodes) {
 			if (node.nodeType === Node.TEXT_NODE) {
-				node.nodeValue = Typography.processString(node.nodeValue, replacementTypes);
+				textNodes.push([ node, text.length ]);
+				text += node.nodeValue;
 			} else if (node.childNodes.length > 0) {
-				Typography.processElement(node, replacementTypes, false);
+				text += Typography.processElement(node, replacementTypes, false);
+			}
+		}
+		text = Typography.processString(text, replacementTypes, { retainLength: true });
+		for (nodeInfo of textNodes)
+			nodeInfo[0].nodeValue = Typography.substringSansSeparators(text, nodeInfo[0].nodeValue, nodeInfo[1]);
+
+		/*	Word breaks cannot be processed non-locally; each text node must be
+			handled individually, due to length increase from added word breaks.
+		 */
+		if (doWordbreaks) {
+			for (node of element.childNodes) {
+				if (node.nodeType === Node.TEXT_NODE) {
+					node.nodeValue = Typography.processString(node.nodeValue, Typography.replacementTypes.WORDBREAKS);
+				} else if (node.childNodes.length > 0) {
+					Typography.processElement(node, Typography.replacementTypes.WORDBREAKS, false);
+				}
 			}
 		}
 
 		//  Transform zero-width spaces into <wbr> tags.
 		if (replaceZeroWidthSpaces)
 			Typography.replaceZeroWidthSpaces(element);
+
+		return text;
 	}
 };
