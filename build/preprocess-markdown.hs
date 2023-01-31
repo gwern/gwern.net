e27f885173cdd260157957726230e3daedcdf4a2
gwern <gwern@gwern.net>
2023-01-31 17:09:13 -0500

    p-m: make usable outside normal workflow

--- build/preprocess-markdown.hs
+++ build/preprocess-markdown.hs
@@ -4,6 +4,7 @@ module Main where
 import Control.Monad (unless)
 import qualified Data.Text as T (unpack)
 import qualified Data.Text.IO as TIO (getContents)
+import System.Directory (setCurrentDirectory)
 
 import Text.Pandoc (def, pandocExtensions, runPure, readerExtensions, readMarkdown, writeHtml5String)
 import Text.Pandoc.Walk (walk)
@@ -26,6 +27,9 @@ main = do originalMarkdown <- TIO.getContents
                  Left e -> error $ show e ++ ": " ++ show originalMarkdown
                  Right output -> cleanAbstractsHTML $ T.unpack output
           putStrLn html
+
+          -- guarantee we are in the right working directory to read the various metadata databases for extracting recommendations from:
+          setCurrentDirectory "/home/gwern/wiki/"
 
           matchList <- GS.singleShotRecommendations html
           unless (matchList == "") $ putStrLn $ "<div class=\"collapse aux-links-append see-also-append\">\n\n<p><strong>See Also</strong>:</p>\n\n" ++ T.unpack matchList ++ "\n</div>"
