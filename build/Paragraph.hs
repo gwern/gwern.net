3e5d6f760dfb4031d6242ed85782ecaaf83c0b37
gwern <gwern@gwern.net>
2023-05-12 19:58:36 -0400

    split out more configs

--- build/Paragraph.hs
+++ build/Paragraph.hs
@@ -3,12 +3,14 @@ module Paragraph where
 
 import Data.List (isInfixOf, intercalate)
 import qualified Data.Text as T (breakOnAll, pack, unpack, Text)
-import Utils (replace, printGreen, cleanAbstractsHTML, trim, toMarkdown, printRed, safeHtmlWriterOptions, anyInfix)
 import Data.FileStore.Utils (runShellCommand)
 import Text.Show.Pretty (ppShow)
 import Text.Pandoc (def, pandocExtensions, readerExtensions, readMarkdown, runPure, writeHtml5String)
 import System.Exit (ExitCode(ExitFailure))
 import qualified Data.ByteString.Lazy.UTF8 as U (toString) -- TODO: why doesn't using U.toString fix the Unicode problems?
+
+import Utils (replace, printGreen, cleanAbstractsHTML, trim, toMarkdown, printRed, safeHtmlWriterOptions, anyInfix)
+import Config.Paragraph as C
 
 -- If a String (which is not HTML!) is a single long paragraph (has no double-linebreaks), call out to paragraphizer.py, which will use GPT-3 to try to break it up into multiple more-readable paragraphs.
 -- This is quite tricky to use: it wants non-HTML plain text (any HTML will break GPT-3), but everything else wants HTML
@@ -32,7 +34,7 @@ processParagraphizer p a = -- the path is necessary to check against the whiteli
 -- Is an annotation (HTML or Markdown) already If the input has more than one <p>, or if there is one or more double-newlines, that means this input is already multiple-paragraphs
 -- and we will skip trying to break it up further.
 paragraphized :: FilePath -> String -> Bool
-paragraphized f a = f `elem` whitelist ||
+paragraphized f a = f `elem` C.whitelist ||
                   paragraphsMarkdown a || blockElements a || length (paragraphsHtml a) > 1
  where
    -- double newlines are only in Markdown strings, and split paragraphs:
@@ -44,5 +46,3 @@ paragraphized f a = f `elem` whitelist ||
    -- annotations are wrapped in a '<p>...</p>' pair, unless they start with another block element; if there are two or more '<p>', then, there are at least two paragraphs (because it must be '<p>...</p> ... <p>...</p>') and it counts as being paragraphized.
    paragraphsHtml :: String -> [(T.Text,T.Text)]
    paragraphsHtml b = T.breakOnAll "<p>" (T.pack b)
-   whitelist :: [String]
-   whitelist = ["/doc/economics/1998-delong.pdf", "/doc/cs/algorithm/1980-rytter.pdf"]
