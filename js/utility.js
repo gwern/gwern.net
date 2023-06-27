35ef3ffd8e046d797a5a8fd8b9fc7238e482a17f
Said Achmiz <said@saidachmiz.net>
2023-06-27 16:30:53 +0000

    Slight tweak to utility function

--- js/utility.js
+++ js/utility.js
@@ -824,7 +824,7 @@ function getQueryVariable(variable) {
 	for (let i = 0; i < vars.length; i++) {
 		let pair = vars[i].split("=");
 		if (pair[0] == variable)
-			return pair[1];
+			return decodeURIComponent(pair[1]);
 	}
 
 	return null;
