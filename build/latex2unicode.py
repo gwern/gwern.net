09fa0b7e463d9aa47a3e2b7b0d59733390c82467
gwern <gwern@gwern.net>
2023-06-30 12:26:53 -0400

    +latex2unicode.py: a LaTeX->Unicode/HTML/CSS OA API script to process ArXiv abstract inline math with & obsolete the drudgery of hand-writing the rewrites in Utils.hs (https://twitter.com/gwern/status/1674168182985179139)

---
+++ build/latex2unicode.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# latex2unicode.py: Convert a simple inline TeX/LaTeX (aimed at ArXiv abstracts) into Unicode+HTML+CSS, using the OA API.
+# Author: Gwern Branwen
+# Date: 2023-06-28
+# When:  Time-stamp: "2023-06-30 12:33:23 gwern"
+# License: CC-0
+#
+# Usage: $ OPENAI_API_KEY="sk-XXX" xclip -o | python latex2unicode.py
+#
+# Typesetting TeX/LaTeX for web browsers is typically a heavyweight operation; even if done server-side, display often requires a lot of CSS+fonts. And then the result looks highly unnatural and clearly 'alien', interrupting reading flow. This is worthwhile for complex equations, where browser typesetting is not up to snuff, but for many in-the-wild TeX uses, the use is often as simple as `$X$`, which would look better as `<em>X</em>` & take megabytes less to render. So it is desirable for simple TeX expressions to convert them to 'native' Unicode/HTML (augmented with a bit of custom CSS to handle things like superscripts-over-subscripts which pop up in integrals/summations/binomials/matrices etc).
+# Unfortunately, TeX is an irregular macro language which is hard to parse and 'compile' to Unicode: it's easy to do many examples, but there's a long tail of weird variables, formatting commands etc, which means that I wind up defining lots of rewrites by hand, even though they are usually pretty 'obvious'. So, quite tedious and unrewarding.
+# However, this is a perfect use-case for GPT models: it is hard to write comprehensive rules for, but is an extremely constrained problem in a domain it knows well which requires processing few tokens, where I can give it many few-shot examples, interrogate it for edge-cases to then write rules/examples for, and the harm of an error is relatively minimal (anyone seriously using an equation will need to read the original anyway, so won't be fooled by a wrong translation).
+# So we write down a list of general rules, then a bunch of specific examples, then ask GPT-4 to translate from TeX to Unicode/HTML/CSS.
+#
+# eg.
+# $ echo 'a + b = c^2' | python3 latex2unicode.py
+# <em>a</em> + <em>b</em> = <em>c</em><sup>2</sup>
+
+import signal
+import sys
+import openai
+
+# define timeout handling:
+def handler(signum, frame):
+    raise TimeoutError("Function call timed out")
+
+# define function to run with timeout:
+def run_with_timeout(func_name, args=(), kwargs={}, timeout=5):
+    func = getattr(openai.ChatCompletion, func_name)
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(timeout)
+    try:
+        result = func(*args, **kwargs)
+    except TimeoutError as e:
+        result = None
+    finally:
+        signal.alarm(0)
+    return result
+
+if len(sys.argv) == 1:
+    input_text = sys.stdin.read().strip()
+else:
+    input_text = sys.argv[1]
+
+prompt = """
+Task: Convert LaTeX inline expressions from ArXiv-style TeX math to inline Unicode+HTML+CSS, for easier reading in web browsers.
+
+Details:
+
+- Convert only if the result is unambiguous.
+- Assume only default environment settings with no redefinitions or uses like `\newcommand` or `\begin`.
+- Do not modify block-level equations, or complex structures such as diagrams or tables or arrays or matrices (eg `\begin{bmatrix}`), or illustrations such as drawn by TikZ or `\draw` , as those require special processing. Do not convert them & simply repeat it if the input is not an inline math expression.
+- If a TeX command has no reasonable Unicode equivalent, such as the `\overrightarrow{AB}`/`\vec{AB}` or `\check{a}` or `\\underline`/`overline` commands in LaTeX, simply repeat it.
+- If a TeX command merely adjusts positioning, size, or margin (such as `\big`/`\raisebox`/`\big`/`\Big`), always omit it from the conversion (as it is probably unnecessary & would need to be handled specially if it was).
+- The TeX/LaTeX special glyphs (`\TeX` & `\LaTeX`) are handled elsewhere; do not convert them, but simply repeat it.
+- Use Unicode entities, eg. MATHEMATICAL CAPITAL SCRIPT O 𝒪 in place of `\mathcal{O}`, and likewise for the Fraktur ones (`\mathfrak`) and bold ones (`\mathbb`). Convert to the closest Unicode entity that exists. Convert symbols, special symbols, mathematical operators, and Greek letters. Convert even if the Unicode is rare (such as  `𝒪`). If there is no Unicode equivalent (such as because there is not a matching letter in that font family, or no appropriate combining character), then do not convert it.
+- If there are multiple reasonable choices, such as  `\approx` which could be represented as `≈` or `~`, choose the simpler-looking one. Do not choose the complex one unless there is some good specific reason for that.
+- For superimposed subscript+superscript, use a predefined CSS span class `subsup`, eg. `(\Delta^0_n)` → `Δ<span class="subsup"><sup>0</sup><sub><em>n</em></sub></span>`. This is also useful for summations or integrals, such as `\int_a^b f(x) dx` → `∫<span class="subsup"><sup><em>b</em></sup><sub><em>a</em></sub> <em>f</em>(<em>x</em>) <em>dx</em>`.
+- For small fractions, use FRACTION SLASH (⁄) to convert (eg. `1/2` or `\frac{1}{2}` → `1⁄2`). Do not use the Unicode fractions like VULGAR FRACTION ONE HALF `½`.
+- For complex fractions which use superscripts or subscripts, multiple arguments etc, do not convert them & simply repeat them. eg. do not convert `\(\frac{a^{b}}{c^{d}}\)`, as it is too complex.
+- Convert roots such as square or cube roots if that would be unambiguous. For example, `\sqrt[3]{8}` → `∛8` is good, but not `\sqrt[3]{ab}` because `∛<em>ab</em>` is ambiguous; do not convert complex roots like `\sqrt[3]{ab}`.
+- Color & styling: if necessary, you may use very simple CSS inline with a `<span style="">` declaration, such as to color something blue using `<span style="color: blue">`.
+- Be careful about dash use: correctly use MINUS SIGN (−) vs EM DASH (—) vs EN DASH (–) vs hyphen (-).
+- More examples: ` O(1)` → ` 𝒪(1)`; `<span class="math inline">\(\mathsf{TC}^0\)</span>` → `<strong>TC</strong><sup>0</sup>`; `<span class="math inline">\(\approx\)</span>` → `~`; `<span class="math inline">\(1-\tilde \Omega(n^{-1/3})\)</span>` → `1 − Ω̃(<em>n</em><sup>−1⁄3</sup>)`; `<span class="math inline">\(\mathbf{R}^3\)</span>` → `𝐑<sup>3</sup>`; `<span class="math inline">\(\ell_p\)</span>` → `𝓁<sub>p</sub>`; `\textcircled{r}` → `ⓡ`; `(\nabla \log p_t\)` → `∇ log <em>p<sub>t</sub></em>`; `\(\partial_t u = \Delta u + \tilde B(u,u)\)` → `∂<sub><em>t</em></sub><em>u</em> = Δ<em>u</em> + <em>B̃</em>(<em>u</em>, <em>u</em>)`; `\(1 - \frac{1}{e}\)` → `"1 − 1⁄<em>e</em>`; `O(\sqrt{T}` → `𝒪(√<em>T</em>)`; `<span class="math inline">\(^\circ\)</span>` → `°`; `<span class="math inline">\(^\bullet\)</span>` → `•`; `6\times 10^{-6}\)` → `6×10<sup>−6</sup>`; `5\div10` → `5 ÷ 10`; `\Pr(\text{text} | \alpha)` → `Pr(text | α)`; `<span class="math inline">\(\hbar\)</span>` → `ℏ`; `\frac{1}{2}`→ `1⁄2`; `\nabla` → `∇`; `<span>\(r \to\infty\)</span>` → `<em>r</em> → ∞`; `\hat{a}` → `â`; \textit{zero-shot}` → `<em>zero-shot</em>`; `\(f(x) = x \cdot \text{sigmoid}(\beta x)\)` → `<em>f(x)</em> = <em>x</em> × sigmoid(β <em>x</em>)`; `\clubsuit` → `♣`; `\textcolor{red}{x}` → `<span style="color: red">x</span>`; `\textbf{bolding}` → `<strong>bolding</strong>`; `\textit{emphasis}` → `<em>emphasis</em>`;
+
+Task example:
+
+Input to convert: `<span class="math inline">\(H\gg1\)</span>`
+Converted output: `<em>H</em> ≫ 1`
+
+Task:
+
+Input to convert: """ + input_text + "Converted output:\n"
+
+messages = [
+    {"role": "system", "content": "You are a skilled research assistant & tasteful typographer."},
+    {"role": "user", "content": prompt}
+]
+
+result = run_with_timeout(
+    "create",
+    kwargs={
+        "model": "gpt-4", # we use GPT-4 because the outputs are short, we want the highest accuracy possible, we provide a lot of examples & instructions which may overload dumber models, and reviewing for correctness can be difficult, so we are willing to spend a few pennies to avoid the risk of a lower model
+        # "model": "gpt-3.5-turbo",
+        "messages": messages,
+        "max_tokens": 30,
+        "temperature": 0
+    },
+    timeout=60
+)
+
+if result is None:
+                          sys.stderr.write("Function call timed out")
+else:
+                          print(result['choices'][0]['message']['content'])
