9e1074fa87927b5bbdb21aa7a944da51c29cc07b
gwern <gwern@gwern.net>
2023-07-20 12:18:03 -0400

    LM: collapse blocks: rm .aux-links-container as apparently unnecessary now that we collapse each item separately

--- build/LinkMetadata.hs
+++ build/LinkMetadata.hs
@@ -4,7 +4,7 @@
                     link, popup, read, decide whether to go to link.
 Author: Gwern Branwen
 Date: 2019-08-20
-When:  Time-stamp: "2023-07-18 20:36:12 gwern"
+When:  Time-stamp: "2023-07-20 12:16:51 gwern"
 License: CC-0
 -}
 
@@ -527,11 +527,10 @@ generateAnnotationBlock truncAuthorsp annotationP (f, ann) blp slp lb =
                 (if null abst then []
                   else [BlockQuote [RawBlock (Format "html") (rewriteAnchors f (T.pack abst') `T.append`
                                                    if (blp++slp++lb)=="" then ""
-                                                   else "<div class=\"aux-links-container\">" `T.append`
+                                                   else
                                                         ((if blp=="" then "" else ("<div class=\"backlinks-append aux-links-append\"" `T.append` " id=\"" `T.append` lidBacklinkFragment `T.append` "\" " `T.append` ">\n<p>[<a class=\"include-replace-container\" href=\"" `T.append` T.pack blp `T.append` "\"><strong>Backlinks</strong></a>:]</p>\n</div>")) `T.append`
                                                          (if slp=="" then "" else ("<div class=\"similars-append aux-links-append collapse\"" `T.append` " id=\"" `T.append` lidSimilarLinkFragment `T.append` "\" " `T.append` ">\n<p>[<a class=\"include-even-when-collapsed include-replace-container\" href=\"" `T.append` T.pack slp `T.append` "\">Similar links for this annotation</a>.]</p>\n</div>")) `T.append`
-                                                          (if lb=="" then "" else ("<div class=\"link-bibliography-append aux-links-append collapse\"" `T.append` " id=\"" `T.append` lidLinkBibLinkFragment `T.append` "\" " `T.append` ">\n<p><a class=\"include-even-when-collapsed include-replace-container\" href=\"" `T.append` T.pack lb `T.append` "\"><strong>Link Bibliography</strong></a>:</p>\n</div>"))) `T.append`
-                                                   "</div>"
+                                                          (if lb=="" then "" else ("<div class=\"link-bibliography-append aux-links-append collapse\"" `T.append` " id=\"" `T.append` lidLinkBibLinkFragment `T.append` "\" " `T.append` ">\n<p><a class=\"include-even-when-collapsed include-replace-container\" href=\"" `T.append` T.pack lb `T.append` "\"><strong>Link Bibliography</strong></a>:</p>\n</div>")))
                                                               )]
                        ])
     where
