d24ed9199b3c737e4e075481d11f6e991eee32ef
Said Achmiz <said@saidachmiz.net>
2023-09-13 22:09:15 +0000

    Temporarily add client-side wordbreak-after-slash insertion

--- js/rewrite.js
+++ js/rewrite.js
@@ -776,6 +776,20 @@ addContentLoadHandler(GW.contentLoadHandlers.aggregateMarginNotes = (eventInfo) 
 /* TYPOGRAPHY */
 /**************/
 
+/***********************************/
+/*	Rectify typography in body text.
+
+	NOTE: This should be temporary. Word breaks after slashes should be added
+	in body text on the back end, at content build time. But that is currently
+	not working, hence this temporary client-side solution.
+	—SA 2023-09-13
+ */
+addContentLoadHandler(GW.contentLoadHandlers.rectifyTypographyInBodyText = (eventInfo) => {
+    GWLog("rectifyTypographyInBodyText", "rewrite.js", 1);
+
+    Typography.processElement(eventInfo.container, Typography.replacementTypes.WORDBREAKS);
+}, "rewrite");
+
 /******************************************************************************/
 /*  Remove extraneous whitespace-only text nodes from between the element parts
     of a .cite (citation element).
