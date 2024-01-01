d576435c64b2df803e0a9685d84fbf2b52d0d747
gwern <gwern@gwern.net>
2024-01-01 17:09:56 -0500

    holidays: +April Fools stub (current plan: Fraktur /doc/cs/css/2019-02-28-gwern-gwernnet-homepage-frakturlinks.png )

--- js/head-GENERATED.js
+++ js/head-GENERATED.js
@@ -2272,83 +2272,83 @@ document.addEventListener("readystatechange", () => {
 /*********************/
 
 /********************************************************************/
-/*	Inject a special page logo image of a specific type (‘halloween’,
-	‘christmas’, etc.). Directory structure and file naming for the
-	specified logo type must match existing holiday logos.
+/*  Inject a special page logo image of a specific type (‘halloween’,
+    ‘christmas’, etc.). Directory structure and file naming for the
+    specified logo type must match existing holiday logos.
  */
 function injectSpecialPageLogo(logoType, options = { }) {
-	let scale = valMinMax(Math.ceil(window.devicePixelRatio), 1, 3);
+    let scale = valMinMax(Math.ceil(window.devicePixelRatio), 1, 3);
 
-	let logoPathname;
-	if (options.randomize) {
-		logoPathname = options.mode
-					   ? `/static/img/logo/${logoType}/${options.mode}/logo-${logoType}-${options.mode}-%R-small-${scale}x.png`
-					   : `/static/img/logo/${logoType}/logo-${logoType}-%R-small-${scale}x.png`;
-	} else {
-		logoPathname = options.mode
-					   ? `/static/img/logo/${logoType}/${options.mode}/logo-${logoType}-${options.mode}-small-${scale}x.png`
-					   : `/static/img/logo/${logoType}/logo-${logoType}-small-${scale}x.png`;
-	}
+    let logoPathname;
+    if (options.randomize) {
+        logoPathname = options.mode
+                       ? `/static/img/logo/${logoType}/${options.mode}/logo-${logoType}-${options.mode}-%R-small-${scale}x.png`
+                       : `/static/img/logo/${logoType}/logo-${logoType}-%R-small-${scale}x.png`;
+    } else {
+        logoPathname = options.mode
+                       ? `/static/img/logo/${logoType}/${options.mode}/logo-${logoType}-${options.mode}-small-${scale}x.png`
+                       : `/static/img/logo/${logoType}/logo-${logoType}-small-${scale}x.png`;
+    }
 
-	//	Temporarily brighten logo, then fade slowly after set duration.
-	let brightenLogoTemporarily = (brightDuration, fadeDuration) => {
-		let logoLink = document.querySelector("#sidebar a.logo");
+    //  Temporarily brighten logo, then fade slowly after set duration.
+    let brightenLogoTemporarily = (brightDuration, fadeDuration) => {
+        let logoLink = document.querySelector("#sidebar a.logo");
 
-		logoLink.classList.add("bright");
-		logoLink.fadeTimer = setTimeout(() => {
-			logoLink.swapClasses( [ "bright", "fading" ], 1);
-			logoLink.fadeTimer = setTimeout(() => {
-				logoLink.classList.remove("fading");
-			}, fadeDuration);
-		}, brightDuration);
+        logoLink.classList.add("bright");
+        logoLink.fadeTimer = setTimeout(() => {
+            logoLink.swapClasses( [ "bright", "fading" ], 1);
+            logoLink.fadeTimer = setTimeout(() => {
+                logoLink.classList.remove("fading");
+            }, fadeDuration);
+        }, brightDuration);
 
-		//	Ensure proper interaction with mouse hover.
-		logoLink.addEventListener("mouseenter", (event) => {
-			logoLink.classList.remove("fading");
-		});
-		logoLink.addEventListener("mouseleave", (event) => {
-			logoLink.classList.remove("fading");
-		});
-	}
+        //  Ensure proper interaction with mouse hover.
+        logoLink.addEventListener("mouseenter", (event) => {
+            logoLink.classList.remove("fading");
+        });
+        logoLink.addEventListener("mouseleave", (event) => {
+            logoLink.classList.remove("fading");
+        });
+    }
 
-	/*	Note that randomAsset() and versionedAssetURL() are defined in misc.js,
-		and so cannot be called prior to this.
-	 */
-	let replaceLogo = (logoImage) => {
-		//	Get new logo URL (random, if need be).
-		if (options.randomize)
-			logoPathname = randomAsset(logoPathname);
-		let versionedLogoURL = versionedAssetURL(logoPathname);
+    /*  Note that randomAsset() and versionedAssetURL() are defined in misc.js,
+        and so cannot be called prior to this.
+     */
+    let replaceLogo = (logoImage) => {
+        //  Get new logo URL (random, if need be).
+        if (options.randomize)
+            logoPathname = randomAsset(logoPathname);
+        let versionedLogoURL = versionedAssetURL(logoPathname);
 
-		//	Create new image element and wrapper.
-		let imageWrapper = newElement("SPAN", {
-			class: "logo-image"
-		});
-		imageWrapper.append(newElement("IMG", {
-			class: "figure-not",
-			src: versionedLogoURL.pathname + versionedLogoURL.search
-		}));
+        //  Create new image element and wrapper.
+        let imageWrapper = newElement("SPAN", {
+            class: "logo-image"
+        });
+        imageWrapper.append(newElement("IMG", {
+            class: "figure-not",
+            src: versionedLogoURL.pathname + versionedLogoURL.search
+        }));
 
-		//	Inject wrapped image.
-		logoImage.replaceWith(imageWrapper);
+        //  Inject wrapped image.
+        logoImage.replaceWith(imageWrapper);
 
-		//	Brighten logo; fade (over 1 second) after 20 seconds.
-		brightenLogoTemporarily(20 * 1000, 1000);
-	};
+        //  Brighten logo; fade (over 1 second) after 20 seconds.
+        brightenLogoTemporarily(20 * 1000, 1000);
+    };
 
-	let logoSelector = "#sidebar .logo-image";
-	let logoImage;
-	if (logoImage = document.querySelector(logoSelector)) {
-		replaceLogo(logoImage);
-	} else {
-		let observer = new MutationObserver((mutationsList, observer) => {
-			if (logoImage = document.querySelector(logoSelector)) {
-				observer.disconnect();
-				replaceLogo(logoImage);
-			}
-		});
-		observer.observe(document.documentElement, { childList: true });
-	}
+    let logoSelector = "#sidebar .logo-image";
+    let logoImage;
+    if (logoImage = document.querySelector(logoSelector)) {
+        replaceLogo(logoImage);
+    } else {
+        let observer = new MutationObserver((mutationsList, observer) => {
+            if (logoImage = document.querySelector(logoSelector)) {
+                observer.disconnect();
+                replaceLogo(logoImage);
+            }
+        });
+        observer.observe(document.documentElement, { childList: true });
+    }
 }
 
 /*  If a special function is provided to apply classes, one should also be
@@ -2356,70 +2356,84 @@ function injectSpecialPageLogo(logoType, options = { }) {
  */
 GW.specialOccasions = [
     [ "halloween", () => isTodayHalloween(), () => {
-		//	Default to dark mode during Halloween.
+        //  Default to dark mode during Halloween.
         DarkMode.defaultMode = "dark";
 
-		//	Different special styles for light and dark mode.
+        //  Different special styles for light and dark mode.
         document.body.classList.remove("special-halloween-dark", "special-halloween-light");
         let specialClass = DarkMode.computedMode() == "light"
                            ? "special-halloween-light"
                            : "special-halloween-dark";
         document.body.classList.add(specialClass);
 
-		//	Replace logo.
-		injectSpecialPageLogo("halloween", { mode: "dark", randomize: true });
+        //  Replace logo.
+        injectSpecialPageLogo("halloween", { mode: "dark", randomize: true });
       }, () => {
         document.body.classList.remove("special-halloween-dark", "special-halloween-light");
       } ],
     [ "christmas", () => isTodayChristmas(), () => {
-		//	Different special styles for light and dark mode.
+        //  Different special styles for light and dark mode.
         document.body.classList.remove("special-christmas-dark", "special-christmas-light");
         let specialClass = DarkMode.computedMode() == "light"
                            ? "special-christmas-light"
                            : "special-christmas-dark";
         document.body.classList.add(specialClass);
 
-		//	Replace logo.
-		injectSpecialPageLogo("christmas", { mode: DarkMode.computedMode(), randomize: true });
+        //  Replace logo.
+        injectSpecialPageLogo("christmas", { mode: DarkMode.computedMode(), randomize: true });
       }, () => {
         document.body.classList.remove("special-christmas-dark", "special-christmas-light");
       } ],
 ];
 
 function isTodayHalloween() {
-	//	The test page is Halloween Town.
-	if (document.body.classList.contains("test-halloween"))
-		return true;
+    //  The test page is Halloween Town.
+    if (document.body.classList.contains("test-halloween"))
+        return true;
 
-    //	Only bother English-speakers with Anglosphere holidays like Halloween:
+    //  Only bother English-speakers with Anglosphere holidays like Halloween:
     let language = window.navigator.userLanguage || window.navigator.language;
     if ("en" == language.slice(0, 2)) { // match ‘en’, ‘en-US’, ‘en-GB’, ‘en-AU’...
         let now = new Date();
         let date = now.toString().slice(4,10);
         let hour = now.getHours();
-        /*	It is a sin to celebrate Halloween while there is daylight; however,
-        	calculating local sunset or local ambient light is too hard (where
-        	would we even get that geolocation or light sensor data from‽), so
-        	we will simply define ‘night’ as >=6PM and <6AM.
+        /*  It is a sin to celebrate Halloween while there is daylight; however,
+            calculating local sunset or local ambient light is too hard (where
+            would we even get that geolocation or light sensor data from‽), so
+            we will simply define ‘night’ as >=6PM and <6AM.
         */
         return (date == "Oct 31" && hour >= 18) || (date == "Nov 01" && hour < 6);
     } else {
-    	return false;
+        return false;
     }
 }
 function isTodayChristmas() {
-	//	The test page is Christmas Town.
-	if (document.body.classList.contains("test-christmas"))
-		return true;
+    //  The test page is Christmas Town.
+    if (document.body.classList.contains("test-christmas"))
+        return true;
 
     let now = new Date();
     let date = now.toString().slice(4,10);
     let hour = now.getHours();
-    /*	Christmas = Christmas Eve + all Christmas Day; Christmas Eve starts in
-    	the evening, so again >=6PM.
+    /*  Christmas = Christmas Eve + all Christmas Day; Christmas Eve starts in
+        the evening, so again >=6PM.
      */
-    return (date == "Dec 24" && hour >= 18) || (date == "Dec 25")
+    return (date == "Dec 24" && hour >= 18) || (date == "Dec 25");
 }
+function isTodayAprilfools() {
+    //  The test page is Fraktur-town
+    if (document.body.classList.contains("test-april-fools"))
+        return true;
+
+    let now = new Date();
+    let month = now.getMonth(); // January is 0, so April will be 3
+    let date = now.getDate();
+    let hour = now.getHours();
+    /*  We don't define April Fools as all-day April 1st,
+        because too early in the morning no one is awake enough for pranks, and after 3PM it's honestly kinda tiresome.
+    */
+    return (month === 3 && date === 1 && hour >= 8 && hour <= 15);
+    }
 
 function applySpecialOccasionClasses() {
     for (occasion of GW.specialOccasions) {
