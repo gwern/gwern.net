cfb9de3b224cab212d938be109e987020151a2e8
Said Achmiz <said@saidachmiz.net>
2024-01-22 01:42:29 +0000

    Fig bugs in SVG processing scripts

--- build/svg_strip_background.php
+++ build/svg_strip_background.php
@@ -15,12 +15,12 @@ foreach ($file_names as $file_name) {
 	$width = $m[2];
 	$height = $m[4];
 
-// 	$path_regexp = '/(<path[^<>]*d="' 
-// 				 . "M-0 -0L{$width} 0L{$width} {$height}L-0 {$height}L0 -0Z" 
-// 				 . '"[^<>]*>)/';
-	$path_regexp = '/(<path[^<>]*d="'
-				 . "M0 0h{$width}v{$height}H0z"
+	$path_regexp = '/(<path[^<>]*d="' 
+				 . "M-0 -0L{$width} 0L{$width} {$height}L-0 {$height}L0 -0Z" 
 				 . '"[^<>]*>)/';
+// 	$path_regexp = '/(<path[^<>]*d="'
+// 				 . "M0 0h{$width}v{$height}H0z"
+// 				 . '"[^<>]*>)/';
 	preg_match($path_regexp, $svg, $m);
 	
 	$svg = preg_replace($path_regexp, '', $svg);
