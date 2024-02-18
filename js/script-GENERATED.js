b5b5fdedcecc8e21b01d41fb4fe663636fa5eb1c
Said Achmiz <said@saidachmiz.net>
2024-02-18 03:50:35 +0000

    Implement lazy loading for video posters

--- js/script-GENERATED.js
+++ js/script-GENERATED.js
@@ -6404,7 +6404,7 @@ Content = {
 											class="${link.classList}"
 											controls="controls" 
 											preload="none" 
-											poster="${posterPathname}"
+											data-video-poster="${posterPathname}"
 											>`
 										+ `<source 
 											src="${link.href}" 
@@ -12939,6 +12939,23 @@ addContentLoadHandler(GW.contentLoadHandlers.relocateThumbnailInAnnotation = (ev
     if (initialFigure)
         container.insertBefore(initialFigure, container.firstElementChild);
 }, "rewrite");
+
+/******************************************************************************/
+/*	There is no browser native lazy loading for <video> tag `poster` attribute,
+	so we implement it ourselves.
+ */
+addContentInjectHandler(GW.contentInjectHandlers.lazyLoadVideoPosters = (eventInfo) => {
+    GWLog("lazyLoadVideoPosters", "rewrite.js", 1);
+
+	eventInfo.container.querySelectorAll("video").forEach(video => {
+		lazyLoadObserver(() => {
+			video.poster = video.dataset.videoPoster;
+		}, video, {
+			root: scrollContainerOf(video),
+			rootMargin: "100%"
+		});
+	});
+}, "eventListeners");
 
 /****************************************************************/
 /*  Account for interaction between image-focus.js and popups.js.
