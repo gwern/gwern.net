b4656a6a1b4ee455fcffdddba313cbbdf178ca0f
gwern <gwern@gwern.net>
2024-02-28 22:58:43 -0500

    continue tweaking tooling for GTX

--- build/preprocess-annotation.sh
+++ build/preprocess-annotation.sh
@@ -3,6 +3,5 @@
 # Read from stdin and process the input suitable for feeding into an Emacs buffer for editing & storing in full/half.gtx.
 cat - | ~/wiki/static/build/preprocess-markdown | \
     pandoc --mathjax --metadata title='Annotation preview' --to=html5 --from=html | \
-    tidy -quiet --show-warnings no --show-body-only auto -indent -wrap 130 \
-         --clean yes --merge-divs no --break-before-br yes --logical-emphasis yes --quote-nbsp no  | \
-    sed 's/^/    /'
+    tidy -quiet --show-warnings no --show-body-only auto -wrap 180 \
+         --clean yes --merge-divs no --break-before-br yes --logical-emphasis yes --quote-nbsp no
