99e66b44c02ef81a015ea3a074c81fdac4b035f9
Said Achmiz <said@saidachmiz.net>
2024-03-13 20:23:45 +0000

    Update dark mode selector tooltip for current macOS

--- js/script-GENERATED.js
+++ js/script-GENERATED.js
@@ -17768,7 +17768,7 @@ DarkMode = { ...DarkMode,
 	/*	Configuration.
 	 */
 	modeOptions: [
-		[ "auto", "Auto", "Auto Light/Dark", "Auto Light/Dark", "Set light or dark mode automatically, according to system-wide setting (Win: Start → Personalization → Colors; Mac: Apple → System-Preferences → General → Appearance; iOS: Settings → Display-and-Brightness; Android: Settings → Display)", "adjust-solid" ],
+		[ "auto", "Auto", "Auto Light/Dark", "Auto Light/Dark", "Set light or dark mode automatically, according to system-wide setting (Win: Start → Personalization → Colors; Mac: Apple → System-Preferences → Appearance; iOS: Settings → Display-and-Brightness; Android: Settings → Display)", "adjust-solid" ],
 		[ "light", "Dark", "Light Mode", "Light Mode", "Light mode at all times (black-on-white)", "sun-solid" ],
 		[ "dark", "Light", "Dark Mode", "Dark Mode", "Dark mode at all times (inverted: white-on-black)", "moon-solid" ]
 	],
