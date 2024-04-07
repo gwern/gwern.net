ecad468d6f399cde9a02fe703f486e7167c12367
gwern <gwern@gwern.net>
2024-04-07 23:30:00 +0800

    Annotation: Gwern.net: rip out most of the ToC munging in favor of as simple as possible, due to persistent bugs & HTML structure errors
    since Achmiz has to rebuild the ToC anyway when transcluding in the JS, there's not too much point in trying to tagsoup & statically compile things as fancily as possible given that it is introducing serious brokenness in many scraped abstracts

--- build/Annotation/Gwernnet.hs
+++ build/Annotation/Gwernnet.hs
@@ -3,10 +3,10 @@ module Annotation.Gwernnet where
 import Control.Monad (when)
 import Data.List (isInfixOf, isPrefixOf, isSuffixOf)
 import qualified Data.ByteString.Lazy.UTF8 as U (toString) -- TODO: why doesn't using U.toString fix the Unicode problems?
-import Text.HTML.TagSoup (isTagOpenName, parseTags, renderOptions, renderTags, renderTagsOptions, Tag(TagClose, TagOpen, TagText))
+-- import Text.HTML.TagSoup (isTagOpenName, parseTags, renderOptions, renderTags, renderTagsOptions, Tag(TagClose, TagOpen, TagText))
 import qualified Data.Text as T (pack, unpack)
 import System.Exit (ExitCode(ExitFailure))
-import Text.Pandoc (runPure, nullMeta, writeHtml5String, Pandoc(..))
+-- import Text.Pandoc (runPure, nullMeta, writeHtml5String, Pandoc(..))
 import Data.FileStore.Utils (runShellCommand)
 import System.FilePath (takeDirectory)
 import Text.Regex.TDFA ((=~))
@@ -15,10 +15,35 @@ import Annotation.PDF (pdf)
 import Image (invertImage)
 import LinkMetadataTypes (MetadataItem, Failure(..), Path)
 import MetadataFormat (checkURL, cleanAbstractsHTML, dateRegex, sectionAnonymousRegex, footnoteRegex, cleanAuthors)
-import Utils (anyInfix, anySuffix, anyPrefix, printGreen, printRed, replace, replaceMany, safeHtmlWriterOptions, sed, sedMany, split, trim)
+import Utils (anyInfix, anySuffix, anyPrefix, printGreen, printRed, replace, replaceMany, sed, split, trim) -- safeHtmlWriterOptions, sedMany
 import Tags (listTagDirectoriesAll, abbreviateTag)
 import LinkAuto (linkAutoHtml5String)
-import Query (truncateTOCHTML)
+-- import Query (truncateTOCHTML)
+
+import Text.HTML.TagSoup (renderTags, parseTags, isTagOpenName, Tag(TagClose,TagOpen, TagText))
+
+-- | Extracts the content of the first div with the specified id and class.
+findDivContent :: String -> Maybe String
+findDivContent html = renderTags <$> extractDivContents 0 (parseTags html)
+  where
+    targetOpenTag = TagOpen "div" [("id", "TOC"), ("class", "TOC")]
+
+    -- Recursively search for the target div, tracking nesting level to find the matching closing tag.
+    extractDivContents :: Int -> [Tag String] -> Maybe [Tag String]
+    extractDivContents _ [] = Nothing  -- End of tags, target not found.
+    extractDivContents level (tag:tags)
+      | tag == targetOpenTag && level == 0 = Just $ takeContent 1 tags
+      | otherwise = extractDivContents level tags
+
+    -- Take tags until the matching closing tag for the target div is found, considering nested divs.
+    takeContent :: Int -> [Tag String] -> [Tag String]
+    takeContent 0 _ = []  -- Closed target div.
+    takeContent level (tag:tags)
+      | tag == TagOpen "div" [] = tag : takeContent (level + 1) tags  -- Found a nested open div.
+      | tag == TagClose "div" && level == 1 = []  -- Found the closing tag of target div.
+      | tag == TagClose "div" = tag : takeContent (level - 1) tags  -- Found a closing tag of a nested div.
+      | otherwise = tag : takeContent level tags  -- Continue with the next tag.
+    takeContent x y = error $ "Gwernnet.hs: findDivContent: takeContent: pattern-amtch failed, which should be impossible: " ++ show x ++ " : " ++ show y
 
 gwern :: Path -> IO (Either Failure (Path, MetadataItem))
 gwern "/doc/index" = gwerntoplevelDocAbstract -- special-case ToC generation of all subdirectories for a one-stop shop
@@ -35,7 +60,7 @@ gwern p | p == "/" || p == "" = return (Left Permanent)
         | p =~ footnoteRegex= return (Left Permanent) -- shortcut optimization: footnotes will never have abstracts (right? that would just be crazy hahaha ・・；)
         | otherwise =
             do let p' = sed "^/" "" $ replace "https://gwern.net/" "" p
-               let indexP = "doc/" `isPrefixOf` p' && "/index" `isInfixOf` p'
+               -- let indexP = "doc/" `isPrefixOf` p' && "/index" `isInfixOf` p'
                printGreen p'
                checkURL p
                (status,_,bs) <- runShellCommand "./" Nothing "curl" ["--silent", "https://gwern.net/"++p', "--user-agent", "gwern+gwernscraping@gwern.net"] -- we strip `--location` because we do *not* want to follow redirects. Redirects creating duplicate annotations is a problem.
@@ -71,10 +96,12 @@ gwern p | p == "/" || p == "" = return (Left Permanent)
                               in ("<figure><img " ++ imgClass ++ " height=\"" ++ h ++ "\" width=\"" ++ w ++ "\" src=\"/" ++ thumbnail' ++ "\" title=\"" ++ thumbnailText ++ "\" alt=\"\" /></figure>")
 
                         let doi = [] -- I explored the idea but DOIs are too expensive & ultimately do little useful
-                        let footnotesP = "<section class=\"footnotes\"" `isInfixOf` b
+                        -- let footnotesP = "<section class=\"footnotes\"" `isInfixOf` b
 
-                        let toc = gwernTOC footnotesP indexP p' f
-                        let toc' = if toc == "<div class=\"columns TOC\"></div>" then "" else toc
+                        let toc = findDivContent b
+                        let toc' = case toc of
+                              Nothing -> ""
+                              Just t -> "<div class=\"columns TOC\">" ++ t ++ "</div>"
 
                         let (sectTitle,gabstract) = gwernAbstract ("/index" `isSuffixOf` p' || "newsletter/" `isPrefixOf` p') p' description toc' f
 
@@ -130,6 +157,7 @@ gwernAbstract _ p' description toc f =
   in if (("#" `isInfixOf` p') && null abstrct) then (t,"") else
        if "scrape-abstract-not" `isInfixOf` renderTags abstrctRw then (t,"") else
          (t,abstrct'''') -- if shortAllowed then (t,abstrct'''') else (t,abstrct'''')
+
 dropToAbstract, takeToAbstract, filterAbstract, dropToBody, dropToSectionEnd, dropToLink, dropToLinkEnd, dropToText :: Tag String -> Bool
 dropToClass, dropToID :: String -> Tag String -> Bool
 dropToClass    i (TagOpen "div" attrs) = case lookup "class" attrs of
@@ -162,42 +190,42 @@ dropToText (TagOpen "em" _) = False
 dropToText (TagClose "em") = False
 dropToText _ = True
 
-gwernTOC :: Bool -> Bool -> String -> [Tag String] -> String
-gwernTOC footnotesP indexP p' f =
-   -- Pandoc declines to add an ID to footnotes section; on Gwern.net, we override this by at compile-time rewriting the <section> to have `#footnotes`:
-  (\tc'' -> if not footnotesP then tc'' else sed "</ul>\n? *</div>$" "<li><a href=\"#footnotes\">Footnotes</a></li></ul></div>" tc'') $
- -- for tags, condense the ToC by removing the See Also & Miscellaneous <h1>s, and the Links wrapper around the individual entries:
- (\tc' -> if not indexP then tc'
-   else sedMany [("</li>\n          \n        </ul>",""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#miscellaneous\"><span>Miscellaneous</span></a>\n          </li>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#miscellaneous\">Miscellaneous</a>\n          </li>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#links\"><span>Links</span></a>\n            <ul>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#links\">Links</a>\n            <ul>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#see-also\"><span>See Also</span></a>\n          </li>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#see-also\">See Also</a>\n          </li>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#link-bibliography\"><span>Link Bibliography</span></a>\n          </li>", ""),
-                 ("<li>\n            <a class=\"id-not\" href=\"#link-bibliography\">Link Bibliography</a>\n          </li>", ""),
-                 ("class=\"footnote-ref\" role=\"doc-noteref\"", "")
-                ] tc') $
- (\tc -> -- add columns class to condense it in popups/tags
-        (if indexP then id else replace "<div class=\"columns\"><div class=\"TOC\">" "<div class=\"columns TOC\">") $
-        -- WARNING: Pandoc generates redundant <span></span> wrappers by abusing the span wrapper trick while removing header self-links <https://github.com/jgm/pandoc/issues/8020>; so since those are the only <span>s which should be in ToCs (...right? [EDIT: no, the subscript citations are]), we'll remove them. TODO: should be fixed in Pandoc HEAD, so remove this after a full upgrade.
-        sed "<span>(.*)</span>" "\\1" $
-        (if '#'`elem`p' then (\t -> let toc = truncateTOC p' t in if toc /= "" then "<div class=\"columns TOC\">" ++ toc ++ "</div>" else "") else replace "<a href=" "<a class=\"id-not\" href=") $
-        -- NOTE: we strip the `id="TOC"`, and all other link IDs on TOC subentries, deliberately because the ID will cause HTML validation problems when abstracts get transcluded into tags/link-bibliographies/backlinks.
-        sed " id=\"[a-z0-9-]+\">" ">" $ replace " id=\"markdownBody\"" "" $ replace " id=\"TOC\"" "" tc) index
-        where
-          index = if length indexType1 > length indexType2 then indexType1 else indexType2
-          indexType1 = replace "markdownBody" "" $ replace "directory-indexes" "" $ replace "columns" "columns TOC" $ renderTagsOptions renderOptions $
-            takeWhile (\e' -> e' /= TagClose "div") $ dropWhile (\e -> e /=  TagOpen "div" [("id","markdownBody"),("class","markdownBody directory-indexes columns")]) f
-          indexType2 = renderTagsOptions renderOptions $
-                       [TagOpen "div" [("class","columns")]] ++
-                       (takeWhile (\e' -> e' /= TagClose "div")  $ dropWhile (\e -> e /= TagOpen "div" [("id","TOC"), ("class","TOC")]) f) ++
-                       [TagClose "div"]
+-- gwernTOC :: Bool -> Bool -> String -> [Tag String] -> String
+-- gwernTOC footnotesP indexP p' f =
+--    -- Pandoc declines to add an ID to footnotes section; on Gwern.net, we override this by at compile-time rewriting the <section> to have `#footnotes`:
+--   (\tc'' -> if not footnotesP then tc'' else sed "</ul>\n? *</div>$" "<li><a href=\"#footnotes\">Footnotes</a></li></ul></div>" tc'') $
+--  -- for tags, condense the ToC by removing the See Also & Miscellaneous <h1>s, and the Links wrapper around the individual entries:
+--  (\tc' -> if not indexP then tc'
+--    else sedMany [("</li>\n          \n        </ul>",""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#miscellaneous\"><span>Miscellaneous</span></a>\n          </li>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#miscellaneous\">Miscellaneous</a>\n          </li>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#links\"><span>Links</span></a>\n            <ul>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#links\">Links</a>\n            <ul>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#see-also\"><span>See Also</span></a>\n          </li>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#see-also\">See Also</a>\n          </li>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#link-bibliography\"><span>Link Bibliography</span></a>\n          </li>", ""),
+--                  ("<li>\n            <a class=\"id-not\" href=\"#link-bibliography\">Link Bibliography</a>\n          </li>", ""),
+--                  ("class=\"footnote-ref\" role=\"doc-noteref\"", "")
+--                 ] tc') $
+--  (\tc -> -- add columns class to condense it in popups/tags
+--         (if indexP then id else replace "<div class=\"columns\"><div class=\"TOC\">" "<div class=\"columns TOC\">") $
+--         -- WARNING: Pandoc generates redundant <span></span> wrappers by abusing the span wrapper trick while removing header self-links <https://github.com/jgm/pandoc/issues/8020>; so since those are the only <span>s which should be in ToCs (...right? [EDIT: no, the subscript citations are]), we'll remove them. TODO: should be fixed in Pandoc HEAD, so remove this after a full upgrade.
+--         sed "<span>(.*)</span>" "\\1" $
+--         (if '#'`elem`p' then (\t -> let toc = truncateTOC p' t in if toc /= "" then "<div class=\"columns TOC\">" ++ toc ++ "</div>" else "") else replace "<a href=" "<a class=\"id-not\" href=") $
+--         -- NOTE: we strip the `id="TOC"`, and all other link IDs on TOC subentries, deliberately because the ID will cause HTML validation problems when abstracts get transcluded into tags/link-bibliographies/backlinks.
+--         sed " id=\"[a-z0-9-]+\">" ">" $ replace " id=\"markdownBody\"" "" $ replace " id=\"TOC\"" "" tc) index
+--         where
+--           index = if length indexType1 > length indexType2 then indexType1 else indexType2
+--           indexType1 = replace "markdownBody" "" $ replace "directory-indexes" "" $ replace "columns" "columns TOC" $ renderTagsOptions renderOptions $
+--             takeWhile (\e' -> e' /= TagClose "div") $ dropWhile (\e -> e /=  TagOpen "div" [("id","markdownBody"),("class","markdownBody directory-indexes columns")]) f
+--           indexType2 = renderTagsOptions renderOptions $
+--                        [TagOpen "div" [("class","columns")]] ++
+--                        (takeWhile (\e' -> e' /= TagClose "div")  $ dropWhile (\e -> e /= TagOpen "div" [("id","TOC"), ("class","TOC")]) f) ++
+--                        [TagClose "div"]
 
-truncateTOC :: String -> String -> String
-truncateTOC p' toc = let pndc = truncateTOCHTML (T.pack (sed ".*#" "" p')) (T.pack toc) in
-                       if null pndc then "" else
-                           case runPure $ writeHtml5String safeHtmlWriterOptions (Pandoc nullMeta (init pndc)) of
-                             Left e -> error ("Failed to compile truncated ToC: " ++ show p' ++ show toc ++ show e)
-                             Right text -> T.unpack text
+-- truncateTOC :: String -> String -> String
+-- truncateTOC p' toc = let pndc = truncateTOCHTML (T.pack (sed ".*#" "" p')) (T.pack toc) in
+--                        if null pndc then "" else
+--                            case runPure $ writeHtml5String safeHtmlWriterOptions (Pandoc nullMeta (init pndc)) of
+--                              Left e -> error ("Failed to compile truncated ToC: " ++ show p' ++ show toc ++ show e)
+--                              Right text -> T.unpack text
--- build/Query.hs
+++ build/Query.hs
@@ -1,7 +1,7 @@
 {- Query.hs: utility module for extracting links from Pandoc documents.
 Author: Gwern Branwen
 Date: 2021-12-14
-When:  Time-stamp: "2024-02-26 21:36:36 gwern"
+When:  Time-stamp: "2024-04-07 17:17:59 gwern"
 License: CC-0
 -}
 
@@ -122,7 +122,7 @@ idRemove (Link (_,a,b) c d)  = Link ("","id-not":a,b) c d
 idRemove x = x
 
 -- truncateTOCHTML "appendix" "<div class=\"columns\" id=\"TOC\"> <ul> <li> <a href=\"#far-from-the-madding-crowd\"><span>Far From the Madding Crowd</span></a> </li> <li> <a href=\"#egypt\"><span>Egypt</span></a> </li> <li> <a href=\"#more\"><span>More</span></a> </li> <li> <a href=\"#are-cats-domesticated\"><span>Are Cats Domesticated?</span></a> <ul> <li> <a href=\"#dysgenics\"><span>Dysgenics</span></a> <ul> <li> <a href=\"#what-is-to-be-done\"><span>What is to be done?</span></a> </li> </ul> </li> </ul> </li> <li> <a href=\"#bibliography\"><span>Bibliography</span></a> </li> <li> <a href=\"#external-links\"><span>External Links</span></a> </li> <li> <a href=\"#appendix\"><span>Appendix</span></a> <ul> <li> <a href=\"#fuzz-testing\"><span>Fuzz Testing</span></a> </li> <li> <a href=\"#toys\"><span>Toys</span></a> </li> </ul> </li> <li><a href=\"#footnotes\"><span>Footnotes</span></a></li></ul></div>"
--- [Plain [Link ("",[],[]) [Span ("",[],[]) [Str "Appendix"]] ("#appendix","")],BulletList [[Plain [Link ("",[],[]) [Span ("",[],[]) [Str "Fuzz",Space,Str "Testing"]] ("#fuzz-testing","")]],[Plain [Link ("",[],[]) [Span ("",[],[]) [Str "Toys"]] ("#toys","")]]]]
+-- [Plain [Link ("",["id-not"],[]) [Span ("",[],[]) [Str "Appendix"]] ("#appendix","")],BulletList [[Plain [Link ("",["id-not"],[]) [Span ("",[],[]) [Str "Fuzz",Space,Str "Testing"]] ("#fuzz-testing","")]],[Plain [Link ("",["id-not"],[]) [Span ("",[],[]) [Str "Toys"]] ("#toys","")]]]]
 truncateTOCHTML :: T.Text -> T.Text -> [Block]
 truncateTOCHTML i toc = let (Pandoc _ blocks) = parseMarkdownOrHTML False toc in idsRemove $ -- we strip IDs from the ToC as unnecessary and leading to collisions with the abstract when the abstract links to sections
                                                                                  filterListBlocksContainingAnchor i blocks
