bed36d7dfe601c0d2a508d5ce586f667d58ab4aa
Said Achmiz <said@saidachmiz.net>
2024-07-06 17:18:29 +0000

    Fix missing condition

--- js/script-GENERATED.js
+++ js/script-GENERATED.js
@@ -14529,7 +14529,8 @@ addContentInjectHandler(GW.contentInjectHandlers.qualifyAnchorLinks = (eventInfo
                 || (   injectingIntoFullPage
                            //  if we’re transcluding a citation (because we merge footnotes)
                     && (   (   eventInfo.source == "transclude"
-                            && link.classList.contains("footnote-ref"))
+                            && link.classList.contains("footnote-ref")
+                            && eventInfo.mergeFootnotes == true)
                            //  if we’re merging a footnote for transcluded content
                         || (   eventInfo.source == "transclude.footnotes"
                             && link.classList.contains("footnote-back")))))) {
