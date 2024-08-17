e393a44f77a1aeaa5c3c675ac7791a0a9421a7ef
gwern <gwern@gwern.net>
2024-08-17 11:03:49 -0400

    lb/bl: links.css/icons.svg: +css tweaks for bl; make central dot larger in bl; replace lb with much thicker SVG version which should be more legible as a link-icon <https://thenounproject.com/icon/list-4184262/>

--- js/head-GENERATED.js
+++ js/head-GENERATED.js
@@ -4159,7 +4159,7 @@ ReaderMode = {
 //  Activate saved mode, once the <body> element is loaded (and classes known).
 doWhenBodyExists(ReaderMode.setMode);
 GW.assetVersions = {
-	"/static/img/icon/icons.svg": "1723905956",
+	"/static/img/icon/icons.svg": "1723907029",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-1x.png": "1707670288",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-2x.png": "1707670332",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-3x.png": "1698113232",
