9802594cd9fd424d1a426f563ca79afdc133ffb4
gwern <gwern@gwern.net>
2024-11-01 13:39:46 -0400

    Inflation: discuss the more extreme extensions of this idea to denominating everything in year-currencies and then NPV stock-index units (which I don't seem to have covered elsewhere yet)

--- build/Inflation.hs
+++ build/Inflation.hs
@@ -4,7 +4,7 @@ module Inflation (nominalToRealInflationAdjuster, nominalToRealInflationAdjuster
 -- InflationAdjuster
 -- Author: gwern
 -- Date: 2019-04-27
--- When:  Time-stamp: "2024-09-06 18:51:27 gwern"
+-- When:  Time-stamp: "2024-11-01 14:10:16 gwern"
 -- License: CC-0
 --
 -- Experimental Pandoc module for fighting <https://en.wikipedia.org/wiki/Money_illusion> by
@@ -16,7 +16,8 @@ module Inflation (nominalToRealInflationAdjuster, nominalToRealInflationAdjuster
 -- <https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3469008> )
 -- Prior art: minimal. Most nominal amounts are never inflation-adjusted, and where they are, the author
 -- usually settles for either replacing the number, using an ad hoc currency unit like '$2012', or
--- verbosely putting it in parentheses, like '$10 ($100 in 2019)'. My approach may have been inspired by the 2008 Wikipedia template: <https://en.wikipedia.org/wiki/Template:Inflation>.
+-- verbosely putting it in parentheses, like '$10 ($100 in 2019)'.
+-- (My approach may have been inspired by the 2008 Wikipedia template: <https://en.wikipedia.org/wiki/Template:Inflation>.)
 --
 -- Years/dates are specified in a variant of my interwiki link syntax (see Interwiki.hs); for example: '[$50]($2000)'
 -- or '[₿0.5](₿2017-01-01)'.
@@ -232,9 +233,17 @@ bitcoinQuery cy date = case M.lookup date db of
 bitcoinUSDExchangeRate :: Int -> M.Map String Double
 bitcoinUSDExchangeRate cy = M.mapWithKey (\dt amt -> inflationAdjustUSD amt (read (take 4 dt)::Int) cy) (M.fromList bitcoinUSDExchangeRateHistory)
 
-{- This general approach could be applied to many other financial assets.
+{- This general approach of automatically inflation-adjusting dollar/₿ amounts to the current nominal (ie. real) amount, rather than default to presenting ever-more-misleading nominal historical amounts, could be applied to many other financial assets.
+As far as almost all financial software is concerned, 'a dollar is a dollar', and as long as credits & debits sum to zero, everything is fine; inflation is, somewhat bizarrely, almost completely ignored.
+Here are 4 ideas of 'inflation adjustments' to make commonly-cited economic numbers more meaningful:
+
+1. rewrite individual stock returns to be always against a standard stock index, rather than against themselves in nominal terms
+2. annotate stock prices on a specific date with their #1 statistic from that date to 'today'
+3. rewrite currencies to year-currencies, with 'real' currency amounts simply being reported in the most recent year-currency
+4. rewrite year-currencies into the NPV of the next-best alternative investment, which represents the compounding opportunity cost
+
 Stock prices would benefit from being reported in meaningful terms like net real return compared to
-an index like the S&P 500, as opposed to being reported in purely nominal terms: how often do we
+an index like the S&P 500 (reinvesting), as opposed to being reported in purely nominal terms: how often do we
 really care about the absolute return or %, compared to the return over alternatives like the
 default baseline of simple stock indexing? Typically, the question is not 'what is the return from
 investing in stock X 10 years ago?' but 'what is its return compared to simply leaving my money in
@@ -243,13 +252,13 @@ numbers like '70%' is actively misleading to the reader: it should actually be '
 to incorporate the enormous opportunity cost of making such a lousy investment like X.
 (See <https://marginalrevolution.com/marginalrevolution/2016/10/performance-pay-nobel.html>.)
 
-Another example of the silliness of not thinking about the use: ever notice those stock tickers in
+Another example of the silliness of not thinking about the *use* of numbers: ever notice those stock tickers in
 financial websites like WSJ articles, where every mention of a company is followed by today's stock
 return ("Amazon (AMZN: 5%) announced Prime Day would be in July")? They're largely clutter: what
 does a stock being up 2.5% on the day I happen to read an article tell me, exactly? But what *could*
 we make them mean? In news articles, we have two categories of questions in mind:
 
-1. how it started: How did the efficient markets *react*?
+1. how it started (how did the efficient markets *react*)?
 
     When people look at stock price movements to interpret whether news is better or worse than
     expected, they are implicitly appealing to the EMH: "the market understands what this means, and
@@ -263,7 +272,7 @@ we make them mean? In news articles, we have two categories of questions in mind
     stock move in the next ~hour (or possibly day)? Then that movement is cached and displayed
     henceforth. It may not be perfect but it's a lot better than displaying stock movements from
     arbitrarily far in the future when the reader happens to be reading it.
-2. How's it been going since then?
+2. How's it been going (since then)?
 
     When we read news, to generalize event studies, we are interested in the long-term outcome.
     "It's a bold strategy, Cotton. Let's see how it works out for them." So, similar to considering
@@ -281,4 +290,29 @@ stocks) to transform 'AMZN' → '[AMZN](!N "2019-04-27")' to inject the current 
 (This could also be done by the CMS automatically on sight, assuming first-seen = writing-date,
 although with Pandoc this has the disadvantage that round-tripping does not preserve the original
 Markdown formatting, and the Pandoc conventions can look pretty strange compared to 'normal'
-Markdown—at least mine, anyway.) -}
+Markdown—at least mine, anyway.)
+
+It would also be useful to make temporal adjustments in regular accounting software, to treat each year of a currency as a foreign currency, with inflation defining the temporal exchange rate. One would have year-currencies like '2019 dollars' vs '2020 dollars' and so on, and compute with respect to a particular target-year like '2024 dollars'. This allows for much more sensible inter-temporal comparisons of expenditures or income, which do not simply reflect (usually unrelated) inflation rate changes and weird expenditure-currency-time-weighted nominal sums, and which would make clearer things like when real income is declining due to lack of raises.
+Accounting software like ledger/hledger already mostly support such things through their foreign currencies, but they don't make it necessarily easy to write. Some syntactic sugar would help. Like '$' is always a shortcut to USD, regardless of whether that is a dollar in 1792 or 2024; but it could be defined as simply referring to the USD in the year of that transaction and be equivalent to a year-currency like '2024$'.
+
+We could go further: one should not hold much currency, because it is a bad investment. If you hold a dollar permanently, you are foregoing a better alternative investment (like a stock index, eg. VTSAX), so you suffer from both inflation *and* severe opportunity cost. Often, the opportunity cost is a lot larger, too.
+Accounting exists reflect the reality of our decisions & financial health: this is the ultimate goal of accounting which goes beyond just a crude list of account credits/debits or list of assets, which inevitably requires building a simple 'model', with judgments about risks and depreciation and opportunity cost (which is why 'cash flow is a fact, all else is opinion').
+But simply listing everything in dollar amounts fails to do this.
+When we compare individual stocks to the S&P 500, why stop there? Why not denominate *everything* in S&P 500 shares?
+This would better reflect our actual choice ("spend on X" vs "save in VTSAX").
+
+(This is also why accounting systems are never feature-complete, and always keep sprouting new ad hoc features & flags & reports, eventually embodying Greenspun's law with their own, often ad hoc and extremely low-quality, built-in programming language. Accounting is not a simple problem of just tracking some movements of a few kinds of assets like 'dollars' or 'cash' in and out of some 'accounts'. Accounting is as complex as the world, because we always want more informative models of our financial position, and have more domain-specific knowledge to encode, and people will always differ about what they think the causal models are or what the future will be like, and this will cause conflicts over 'accounting'. Accounting is ultimately for *decision-making*: a failure to make this explicit will lead to many confusions & illusions in understanding the purpose of accounting or things like 'GAAP'. (See also <https://gwern.net/research-criticism>.) Accounting systems should never delude themselves about this intrinsic complexity by pretending it doesn't exist and trying to foist it onto the users, but accept that they have to handle it and provide real solutions, like being built with a properly supported language like a Lisp or Python, instead of fighting a rearguard action by adding on features ad hoc as users can ram them through to meet their specific needs.)
+
+Like the year-currencies, this is fine as far as it goes, but it suffers from its own version of 'nominal' vs 'real', by omitting a kind of intrinsic 'deflation' from the opportunity cost of compounding: if we simply do it the default way by defining a daily VTSAX exchange rate and inputting everything in year-currencies, which get converted to final VTSAX numbers for reports, we do take into account the increase in value of a VTSAX share over time, but we do not take into account something fairly important—the *income* from the index.
+This would systematically bias reports by when a share was purchased: older shares are more valuable, assuming we buy-and-hold, and especially if we reinvest that.
+(VTSAX pays out relatively little as dividends, so the problem isn't so big, but for other alternatives, like bonds, this might be wildly misleading.)
+
+We presumably do track income from all investments properly, and so indirectly the opportunity cost is reflected in the balance sheets; the issue is not that it leads to wrong final numbers, but that it obscures the financial reality.
+That of course is true of everything in accounting (sooner or later it all shows up in the assets or cashflow—fail to track depreciation properly and eventually there will be a large expensive expense for replacement/repair; fail to buy insurance or self-insure, and an expensive risk will happen etc), and the point of accounting is to *show* such things beforehand, and let us see the costs & benefits *now*.
+
+So the daily VTSAX share price (mark-to-market) should instead be treated as mark-to-maturity (ie. we plan to do something like "hold it until the year 202X, after which we will sell it during retirement"), and the daily mark reflects the predicted NPV of holding VTSAX for Y years while reinvesting & compounding.
+The further back in time the VTSAX share, the more the 'exchange rate' reflects the future income & compounding (because we require several 2024 VTSAX shares to equal the value of 1 VTSAX share we bought in 2000, because that 2000 share *turned into* several 2024 VTSAX shares all on its own).
+Then we get correct real opportunity-costs of expenses over time: our decision to spend $10 on Beanie Babies in 2000 correctly reflects the real long-term cost to us of not putting that money into an index, and we can compute out the actual (or predicted) cost at any given date, and better see whether that asset performed well or the expense was justified. (*Did* investing in those Beanie Babies beat the alternative, or did our asset total gradually wither each year VTSAX outperformed and in retrospect the decision got worse and we refused to realize the loss? *Did* that $10 of ice cream as a teen make us happier than $100 in retirement we can't spend on anything we care about?)
+This requires a bit more implementation effort, but still is not too hard: the target date can be specified by the user upfront once, and a simple, fair extrapolation of future income/returns used (eg. a simple average of all historical ones). If the user has so much that they can't save tax-free, a fudge factor can be added for taxes (updated after each actual tax filing if the user desires the greater accuracy). As the daily VTSAX share prices are updated however they are normally, the NPV estimate can be updated by the ledger software without any further work from the user. And '$' doesn't need to be further modified, it simply remains a year-currency like the previous idea.
+So for the user, this should be scarcely any more difficult than normal accounting software usage: one inputs transactions every day in a normal dollar amount, the stock price is updated by a script or API, and the main user-visible change is defaulting to 'VTSAX_202X' units instead of '2024$' units, unless the user asks for '2024$' reports etc.
+-}
