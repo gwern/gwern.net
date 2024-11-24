gwern/gwern.net

commit 2a09e2e413e947e22cfea1dd778e850c14600a33

local archives: backtrack on aggressiveness of local archive link rewriting; we will keep the original link and provide the archive link in addition to
The full rewrite has proven to be problematic both for readers, who wind up linking my archives elsewhere (which was not something I wanted them to do), and also for me, as it makes writing annotations a pain when I cannot copy links from the live website but have to manually look up the canonical URL. Instead, we will link links like a normal site, and provide the archive versions as supplements like '[ARCHIVE]' etc.

build/LinkIcon.hs
@@ -63,7 +63,7 @@ import qualified Config.LinkIcon as C (prioritizeLinkIconMin, prioritizeLinkIcon
 -- maybe that's a vestigial concern?
 -- TODO: refactor into multiple functions, like 'linkIconOrg', 'linkIconQuad' etc, and then move into Config.LinkIcon:
 linkIcon :: Inline -> Inline
-linkIcon x@(Link (_,cl,attributes) _ (u, _))
+linkIcon x@(Link (_,cl,_) _ (u, _))
  -- Short-circuits for manual control (one can either disable icons with a `[Foo](URL){.icon-not}`
  -- class, or specify a preferred icon on a link, like `[Foo](URL){.link-icon="deepmind"
  -- .link-icon-type="svg"}` by specifying the attributes directly), or define a global URL/(link
@@ -435,14 +435,10 @@ linkIcon x@(Link (_,cl,attributes) _ (u, _))
  where u', u'' :: T.Text -> Bool
        -- simplest check for string anywhere; note that if it is a full domain name like `https://foo.com` (intended to match `https://foo.com/xyz.html`), then it will *not* match when the local-archive code fires and the URL gets rewritten to "/doc/foo.com/$HASH.html". So we error out if the user tries this, having forgotten that u' ≠ u'' in that respect.
        u' v = if "http://" `T.isPrefixOf` v || "https://" `T.isPrefixOf` v then error ("LinkIcon.hs: Overly strict prefix in infix matching (u'): " ++ show u ++ ":" ++ show v) else
-         if originalURL=="" then v `T.isInfixOf` u else v `T.isInfixOf` originalURL
+         v `T.isInfixOf` u
        -- more stringent check, matching exactly the domain name:
        u'' v = if "http://" `T.isPrefixOf` v || "https://" `T.isPrefixOf` v then error ("LinkIcon.hs: Overly strict prefix in infix matching (u''): " ++ show u ++ ":" ++ show v) else
-                 if originalURL=="" then isHostOrArchive v u else isHostOrArchive v originalURL
-       originalURL :: T.Text
-       originalURL = case lookup "data-url-original" attributes of
-                       Nothing -> ""
-                       Just url -> url
+                 isHostOrArchive v u
        aI :: T.Text -> T.Text -> Inline
        aI = addIcon x
        iE :: [T.Text] -> Bool

build/LinkArchive.hs
@@ -2,7 +2,7 @@
                    mirror which cannot break or linkrot—if something's worth linking, it's worth hosting!
 Author: Gwern Branwen
 Date: 2019-11-20
-When:  Time-stamp: "2023-07-10 22:31:04 gwern"
+When:  Time-stamp: "2023-08-09 22:14:59 gwern"
 License: CC-0
 Dependencies: pandoc, filestore, tld, pretty; runtime: SingleFile CLI extension, Chromium, wget, etc (see `linkArchive.sh`)
 -}
@@ -115,7 +115,7 @@ import Data.ByteString.Char8 (pack, unpack)
 import System.FilePath (takeFileName)
 import System.Directory (doesFileExist)
 
-import Utils (writeUpdatedFile, printGreen, printRed, addClass, currentDay)
+import Utils (writeUpdatedFile, printGreen, printRed, currentDay)
 import qualified Config.LinkArchive as C (whiteList, transformURLsForArchiving, transformURLsForLinking, archivePerRunN, archiveDelay, isCheapArchive)
 
 type ArchiveMetadataItem = Either
@@ -136,12 +136,9 @@ localizeLink adb archivedN x@(Link (identifier, classes, pairs) b (targetURL, ta
   if C.whiteList (T.unpack targetURL) || "archive-not" `elem` classes then return x else
     do targetURL' <- rewriteLink adb archivedN $ T.unpack targetURL
        if targetURL' == T.unpack targetURL then return x -- no archiving has been done yet, return original
-       else do -- rewrite & annotate link with local archive:
-         let padding = if targetDescription == "" then "" else " "
-         let targetDescription' = T.unpack targetDescription ++ padding ++ "(Original URL: " ++ T.unpack targetURL ++ " )"
-         -- specify that the rewritten links are mirrors & to be ignored:
-         let archiveAttributes = [("rel", "archived alternate nofollow"), ("data-url-original", T.pack (C.transformURLsForLinking (T.unpack targetURL)))]
-         let archivedLink = addClass "archive-local" $ Link (identifier, classes, pairs++archiveAttributes) b (T.pack ('/':targetURL'), T.pack targetDescription')
+       else do -- annotate link with data attribute specifying with local archive:
+         let archiveAttributes = [("data-url-archive", T.pack ('/':targetURL'))]
+         let archivedLink = Link (identifier, classes, pairs++archiveAttributes) b (T.pack (C.transformURLsForLinking (T.unpack targetURL)), targetDescription)
          return archivedLink
 localizeLink _ _ x = return x
 

build/sync-gwern.net.sh
@@ -2,7 +2,7 @@
 
 # Author: Gwern Branwen
 # Date: 2016-10-01
-# When:  Time-stamp: "2023-08-05 21:40:16 gwern"
+# When:  Time-stamp: "2023-08-09 22:14:42 gwern"
 # License: CC-0
 #
 # sync-gwern.net.sh: shell script which automates a full build and sync of Gwern.net. A simple build
@@ -250,8 +250,7 @@ else
 
     bold "Stripping compile-time-only classes unnecessary at runtime…"
     cleanClasses () {
-        sed -i -e 's/class=\"\(.*\)archive-local \?/class="\1/g' \
-               -e 's/class=\"\(.*\)archive-not \?/class="\1/g' \
+        sed -i -e 's/class=\"\(.*\)archive-not \?/class="\1/g' \
                -e 's/class=\"\(.*\)id-not \?/class="\1/g' \
                `# TODO: revert 9f246da03503b3c20d3c38eecd235b5aa7caa0b3 and remove .backlink-not clutter once all link-ID problems are resolved` \
                -e 's/class=\"\(.*\)link-annotated-not \?/class="\1/g' \
@@ -411,7 +410,7 @@ else
     ## Whitelist of HTML classes which are authorized for use. Any new classes should be added here.
     λ(){ find metadata/annotation/ -name "*.html" \
              | shuf | xargs --max-procs=0 --max-args=500 ./static/build/htmlClassesExtract.py | tr ' ' '\n' | sort -u | \
-             grep -E --invert-match -e '^see-also-append$' -e '^archive-local$' -e '^archive-not$' -e '^author$' -e '^full-authors-list$' -e '^aux-links$' -e '^backlink-not$' \
+             grep -E --invert-match -e '^see-also-append$' -e '^archive-not$' -e '^author$' -e '^full-authors-list$' -e '^aux-links$' -e '^backlink-not$' \
                    -e '^backlinks$' -e '^backlinks-append$' -e 'aux-links-append' -e '^bash$' -e '^Bash$' -e '^book-review-author$' \
                    -e '^book-review-date$' -e '^book-review-rating$' -e '^book-review-title$' -e '^cite-author$' -e '^cite-author-plural$' \
                    -e '^cite-date$' -e '^date$' -e '^display$' -e '^email$' -e '^external-page-embed$' -e '^id-not$' -e '^include$' \

commit ccd9201dd63ff94a92e451286fa4d37aba81618f

LI: restore handling of local vs canonical URL swapping (broken by 2a09e2e413e947e22cfea1dd778e850c14600a33)

build/Config/LinkIcon.hs
@@ -92,13 +92,13 @@ u'' ""  v  = error $ "Config.LinkIcon.u'': empty string passed as argument; url 
 u'' url "" = error $ "Config.LinkIcon.u'': empty string passed as argument; url was " ++ show url ++ ", domain was ''"
 u'' url v = if "http://" `T.isPrefixOf` v || "https://" `T.isPrefixOf` v
             then error ("Config.LinkIcon: Overly strict prefix in infix matching (u''): " ++ show url ++ ":" ++ show v)
-            else isHostOrArchive v url
+            else Utils.isHostOrArchive v url
 
 iE :: T.Text -> [T.Text] -> Bool
 iE ""  args = error $ "Config.LinkIcon.iE: passed an empty string as the first argument; rest were: " ++ show args
-iE url args = elem (T.drop 1 $ extension url) args
+iE url args = T.drop 1 (extension url) `elem` args
 aU', aU'' :: T.Text -> [T.Text] -> Bool
-aU'  url = any (u' url)
+aU'  url = any (u'  url)
 aU'' url = any (u'' url)
 
 -- The URL matches:
@@ -528,7 +528,7 @@ linkIconRulesFiletypes u
  | iE u ["epub"] = ("EPUB", "text,quad,sans", "#87ba11") -- color: neon green <https://commons.wikimedia.org/wiki/File:Epub_logo.svg>
  | "/static/" `T.isPrefixOf` u && hasExtension ".html" u  = ("code", "svg", "")
  | isLocal u && hasExtension ".php" u                     = ("code", "svg", "#787cb4") -- color: light purple <https://commons.wikimedia.org/wiki/File:PHP-logo.svg>
- | aU' u [".pdf", ".PDF", "/pdf", "type=pdf", "pdfs.semanticscholar.org", "citeseerx.ist.psu.edu", "pdfs.semanticscholar.org", "www.semanticscholar.org"] = ("pdf", "svg", redAdobe) -- color: red (Adobe)
+ | aU' u [".pdf", ".PDF", "/pdf", "type=pdf", "pdfs.semanticscholar.org", "citeseerx.ist.psu.edu", "pdfs.semanticscholar.org", "www.semanticscholar.org"] = ("pdf", "svg", redAdobe) -- color: red (Adobe); NOTE: we do not attempt to check for PDFs very thoroughly because we assume that there are no treacherous URLs or that they are covered by LinkArchive mirroring PDFs locally by default to a '/doc/www/.../$HASH.pdf' URL which will match this reliably.
  | otherwise = ("", "", "")
 
 ------------------------------------------------------------------------------------------

build/LinkIcon.hs
@@ -57,16 +57,10 @@ import qualified Config.LinkIcon as C (prioritizeLinkIconMin, prioritizeLinkIcon
 -- In cases of local archive links, matches on the `/doc/www/$DOMAIN/$ARCHIVE.html` aren't necessarily *exactly*
 -- as powerful; local archives deliberately throw away sub-directory structure for simpler addresses, so 2 matches for
 -- 'foo.com/bar/*' and 'foo.com/quux/*' would collide when trying to match just '/doc/www/foo.com/$ARCHIVE.html'.
--- For this case, we detect & exploit the `data-original-URL` attribute which is around for just such problems,
+-- For this case, we detect & exploit the `data-url-original` attribute which is around for just such problems,
 -- and we run matches on the original URL, and everything should work as expected then.
---
--- TODO: the PDF checks are incomplete (and only look for ".pdf" essentially) but it would require IO or perhaps
--- a caching database to actually detect what MIME type a live URL returns, which is a PITA, and since I'm trying
--- to remove all weird non-standard PDFs and host locally all PDFs with clean names & extensions,
--- maybe that's a vestigial concern?
--- TODO: refactor into multiple functions, like 'linkIconOrg', 'linkIconQuad' etc, and then move into Config.LinkIcon:
 linkIcon :: Inline -> Inline
-linkIcon x@(Link (_,cl,_) _ (u, _))
+linkIcon x@(Link (_,cl,attributes) _ (u, _))
  -- Short-circuits for manual control (one can either disable icons with a `[Foo](URL){.icon-not}`
  -- class, or specify a preferred icon on a link, like `[Foo](URL){.link-icon="deepmind"
  -- .link-icon-type="svg"}` by specifying the attributes directly), or define a global URL/(link
@@ -85,7 +79,11 @@ linkIcon x@(Link (_,cl,_) _ (u, _))
  | "directory-indexes-downwards" `elem` cl = addIcon x ("arrow-down-right", "svg", "")
  | "directory-indexes-sideways"  `elem` cl = addIcon x ("arrow-right", "svg", "")
 
- | otherwise = removeIconDuplicate $ addIcon x $ C.linkIconRules u
+ | otherwise = removeIconDuplicate $ addIcon x $ C.linkIconRules originalURL
+ where originalURL :: T.Text -- NOTE: all rules are defined in terms of the original canonical URL, without local archives in mind. So to cooperate with LinkArchive, we must swap the target if LA swapped it first:
+       originalURL = case lookup "data-url-original" attributes of
+                       Nothing   -> u
+                       Just url -> url
 linkIcon x = x
 
 -- Periodically like newspapers are often referenced by their abbreviation, which may also be their text link-icon;
@@ -99,7 +97,7 @@ removeIconDuplicate x@(Link (_,_,kvs) text _) = let iconType = filter (\(k,v) ->
                                                 in if null iconType then x else
                                                      let iconText = head $ map snd $ filter (\(k,_) -> k == "link-icon") kvs
                                                          text' = inlinesToText text
-                                                     in if iconText /= text' && not (snd (head iconType) == "svg") then x else removeIcon x
+                                                     in if iconText /= text' && (snd (head iconType) /= "svg") then x else removeIcon x
 removeIconDuplicate x = x
 
 -- whether a Link has a link-icon set already; errors out if the attribute keys are set but have empty values (to help guard against that possible error)