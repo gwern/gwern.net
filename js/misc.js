8ab4b71926389144d5c3745f5fa2c51404d63ee8
Said Achmiz <said@saidachmiz.net>
2024-12-02 19:43:51 +0000

    Add server logging call for back-to-top link usage

--- js/misc.js
+++ js/misc.js
@@ -1626,6 +1626,11 @@ if (GW.isMobile() == false) doWhenPageLoaded(() => {
     });
     GW.backToTop.addEventListener("click", (event) => {
         GW.backToTop.style.transition = "";
+
+		/*	Log usage. (Temporary.)
+				— SA 2024-12-02
+		 */
+		GWServerLogError(location.href + "--used-back-to-top-link");
     });
 });
 
