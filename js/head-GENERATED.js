81e09e0a4d376596c775b32aa4062ee1aaf65e3f
Said Achmiz <said@saidachmiz.net>
2024-12-04 23:40:51 +0000

    Add popup minimization feature

--- js/head-GENERATED.js
+++ js/head-GENERATED.js
@@ -4420,7 +4420,7 @@ ReaderMode = {
 //  Activate saved mode, once the <body> element is loaded (and classes known).
 doWhenBodyExists(ReaderMode.setMode);
 GW.assetVersions = {
-	"/static/img/icon/icons.svg": "1733032993",
+	"/static/img/icon/icons.svg": "1733355651",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-1x.png": "1707794185",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-2x.png": "1707794185",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-3x.png": "1698080524",
