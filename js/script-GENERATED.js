2f28e5bac984d8046736e795d27a9f0eaa191868
Said Achmiz <said@saidachmiz.net>
2025-05-17 19:30:09 +0000

    Fix page title bug on /ref/ pages

--- js/script-GENERATED.js
+++ js/script-GENERATED.js
@@ -14659,9 +14659,9 @@ addContentLoadHandler(GW.contentLoadHandlers.loadReferencedIdentifier = (eventIn
 	};
 
 	let updatePageTitleElements = (newTitleHTML) => {
-		eventInfo.document.querySelectorAll("title, header h1").forEach(element => {
-			element.replaceChildren(newDocument(newTitleHTML));
-		});
+		let newPageTitle = newDocument(newTitleHTML);
+		eventInfo.document.querySelector("title")?.replaceChildren(newPageTitle.textContent);
+		eventInfo.document.querySelector("header h1")?.replaceChildren(newPageTitle);
 	};
 
 	let injectHelpfulErrorMessage = (errorMessageHTML) => {
