bd24cc84f02c2ac1ff4447083629aa6d0701b9f1
gwern <gwern@gwern.net>
2025-11-02 17:09:02 -0800

    htmlClassesExtract.py: data-attributes are also risky in terms of typos, so we'll start a whitelist for that too.

--- build/htmlClassesExtract.py
+++ build/htmlClassesExtract.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# htmlClassesExtract.py: extract all CSS classes and data-attribute keys from HTML files
+# Author: Gwern Branwen
+# Date: 2022-09-10
+# When:  Time-stamp: "2025-11-02 17:08:12 gwern"
+# License: CC-0
+#
+# BeautifulSoup utility to parse HTML & print all HTML classes and the data-attribute keys.
+# The usecase is to run over compiled corpus to extract all HTML classes and filter through a grep whitelist of known classes; this frequently catchs typos and unnecessary classes.
+# We check both classes & data-attribute keys for 'unknown' entries; we skip the data-attribute values because by design they will vary far too much to easily check.
+# (We use htmlClassesExtract.py in `sync.sh`; the whitelist serves as additional documentation for Gwern.net features.)
+#
+# Usage: $ python htmlClassesExtract.py file1.html file2.html ...
+#
+# Examples:
+#
+# $ python htmlClassesExtract.py index.html
+# TOC
+# abstract smallcaps-not dropcap-not
+# ...
+# data-filesize-bytes
+# data-link-icon
+# ...
 
 from bs4 import BeautifulSoup
-import requests
 import sys
 
-
-# class list set
 class_list = set()
+data_attr_list = set()
 
-for fileIndex in range(1, len(sys.argv)):
+for file_index in range(1, len(sys.argv)):
+    with open(sys.argv[file_index], "r") as f:
+        page = f.read()
 
-    page = open(sys.argv[fileIndex], "r").read()
-
-    # parse html content
     soup = BeautifulSoup(page, 'html.parser')
 
-    # get all tags
-    tags = {tag.name for tag in soup.find_all()}
+    # iterate all elements once
+    for element in soup.find_all():
+        if element.has_attr("class"):
+            class_list.add(" ".join(element['class']))
 
-    # iterate all tags
-    for tag in tags:
+        for attr in element.attrs:
+            if attr.startswith("data-"):
+                data_attr_list.add(attr)
 
-        # find all element of tag
-        for i in soup.find_all(tag):
+# print("Classes:")
+for val in sorted(class_list):
+    print(f"{val}")
 
-            # if tag has attribute of class
-            if i.has_attr("class"):
-
-                if len(i['class']) != 0:
-                    class_list.add(" ".join(i['class']))
-
-for val in class_list:
-    print(val)
+# print("\nData attributes:")
+for attr in sorted(data_attr_list):
+    print(f"{attr}")
