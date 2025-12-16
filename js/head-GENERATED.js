b88c444b501359c39683b7cf4c86cbd44ba92c02
Said Achmiz <said@saidachmiz.net>
2025-12-16 21:02:22 +0000

    Optimize and bug-fix The Onion link icon

--- js/head-GENERATED.js
+++ js/head-GENERATED.js
@@ -6461,7 +6461,7 @@ GW.notificationCenter.addHandlerForEvent("ReaderMode.didLoad", (eventInfo) => {
 	ReaderMode.setMode();
 }, { once: true });
 GW.assetVersions = {
-	"/static/img/icon/icons.svg": "1761617795",
+	"/static/img/icon/icons.svg": "1765918942",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-1x.png": "1734481578",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-2x.png": "1734481578",
 	"/static/img/logo/christmas/dark/logo-christmas-dark-1-small-3x.png": "1734481578",
