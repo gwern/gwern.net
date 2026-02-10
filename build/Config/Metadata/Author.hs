630d25cbf7f865ac8b3c08cc83eda8f021949937
gwern <gwern@gwern.net>
2026-02-10 00:07:02 -0800

    fix up some tags per Claude-4.6-opus feedback on reviewing /doc/index

--- build/Config/Metadata/Author.hs
+++ build/Config/Metadata/Author.hs
@@ -2162,6 +2162,7 @@ authorLinkDB = M.fromList $
     , ("GPT-5.3-Codex", "https://openai.com/index/introducing-gpt-5-3-codex/")
     , ("Leslie Stephen Coles", "https://en.wikipedia.org/wiki/L._Stephen_Coles")
     , ("Coral Osborne", "https://substack.com/@coralosborne")
+    , ("Jacob Bacharach", "https://jacobbacharach.com/")
     ]
 
 -- Config tests: none, tested via `authorLinkDB` as a whole
@@ -3453,4 +3454,5 @@ authorWpLinkDB = setLike
     , "Friedrich Nietzsche", "William Bialek", "E. E. Cummings", "Simon Willison", "Karen Greenlee"
     , "Constance Reid", "Julia Robinson", "Ianthi-Maria Tsimpli", "Graeme Mitchison", "Ravi Vakil", "Ross Koppel"
     , "Bruce G. Lindsay", "Anne Chao", "John A. Hostetler", "Brian Moriarty", "Archibald MacLeish", "Michael Drew"
-    , "Herbert H. Clark", "Jean E. Fox Tree", "Mikhail Bulgakov", "David Shor", "Rudolf Sloboda", "Frank Bidart", "Gary McGraw"]
+    , "Herbert H. Clark", "Jean E. Fox Tree", "Mikhail Bulgakov", "David Shor", "Rudolf Sloboda", "Frank Bidart"
+    , "Gary McGraw", "Liisa Keltikangas-Järvinen"]
